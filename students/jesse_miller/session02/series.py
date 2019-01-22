96bde7a15a7bd42ad5748d80ddbb4000236240f8
UncleanlyCleric <junyatwin@gmail.com>
2019-01-22 09:34:00 -0800

    session2 exercise series.py placed my calculations for the number series in the template.  Starting sum process

--- students/jesse_miller/session02/series.py
+++ students/jesse_miller/session02/series.py
@@ -1,17 +1,26 @@
 #!/usr/local/bin/python3
-"""
-a template for the series assignment
-"""
-
+import sys
+"""import sys for .argv input"""
+n = int(sys.argv[1])
 
 def fibonacci(n):
-    """ compute the nth Fibonacci number """
-    pass
+   """This is a recusive loop to calc the sequence"""
+   if n <= 1:
+	   return n
+   else:
+	   return(fibonacci(n-1) + fibonacci(n-2))
 
 
 def lucas(n):
-    """ compute the nth Lucas number """
-    pass
+    """This is a recusive loop to calc the sequence"""
+    if n < 0:
+        return None
+    elif n == 0:
+        return 2
+    elif n == 1:
+        return 1
+    else:
+        return(lucas(n-1) + lucas(n-2))
 
 
 def sum_series(n, n0=0, n1=1):
