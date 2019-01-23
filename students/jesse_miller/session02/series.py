5b6eab1023b4f2ba8dc36e41f565c32630e4d706
UncleanlyCleric <junyatwin@gmail.com>
2019-01-23 09:28:40 -0800

    session2 exercise series.py Found a better way, and realized that assert is doing the job of user input.

--- students/jesse_miller/session02/series.py
+++ students/jesse_miller/session02/series.py
@@ -1,7 +1,9 @@
 #!/usr/local/bin/python3
-import sys
-"""import sys for .argv input"""
-n = int(sys.argv[1])
+#import sys
+#"""import sys for .argv input"""
+#n = int(sys.argv[1])
+"""Realized once I worked through sum_series that I actually didn't need user
+input"""
 
 def fibonacci(n):
    """This is a recusive loop to calc the sequence.  The initial test is to make
@@ -40,22 +42,23 @@ def sum_series(n, n0=0, n1=1):
     Once generalized that way, sum_series(n, 0, 1) should be equivalent to fibonacci(n).
     And sum_series(n, 2, 1) should be equivalent to lucas(n).
     """
+    sum = n0
     if n < 0:
-        return None
+        sys.exit("We are trying to find n less than 0 which is bad.")
     elif n == 0:
-        return n0
-    elif n == 1:
-        return n1
+        return sum
     else:
-        return sum_series(n - 1, n0 , n1) + sum_series(n - 2, n0 , n1)
-    pass
+        n -= 1
+        sum = sum + n1
+        return sum_series(n, n1, sum)
 
 """So, at first I couldn't wrap my head around what was being asked here.  I
 ended up talking with a friend who helped me figure out what was being asked.
 What I ended up doing was taking the same if/elif hirarchy I used for lucas,
 and adapting it to here.  I'm almost positive that there's a better way to
 write all of this, but I'm still sort of wrapping my brain around Python's
-seemingly simpler logic."""
+seemingly simpler logic.  Later I played and found a shorter code brick that
+would also work."""
 
 if __name__ == "__main__":
     # run some tests
