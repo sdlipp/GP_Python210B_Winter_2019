72f7711722d683656f888b11d9cbb77ba9339f45
UncleanlyCleric <junyatwin@gmail.com>
2019-01-29 09:05:11 -0800

    Created all four script studs, and in slicing_lab I have the code for every other removed working

---
+++ students/jesse_miller/session03/slicing_lab.py
@@ -0,0 +1,25 @@
+#!/usr/local/bin/python3
+def exchange_first_last(seq):
+    return a_new_sequence
+
+def every_other_removed(seq):
+    result = ""
+    for i in range(len(seq)):
+        if i % 2 == 0:
+            result = result + seq[i]
+    return result
+
+def first_last_four(seq):
+    return a_new_sequence
+
+def reversed(seq):
+    return a_new_sequence
+
+def first_last_middle(seq):
+    return a_new_sequence
+
+a_string = "a guitar has strings that's close"
+a_tuple = (2, 54, 13, 12, 5, 32, 3, 82)
+
+assert every_other_removed(a_string) == "agia a tig htscoe"
+print("Test Passed - every other removed")
