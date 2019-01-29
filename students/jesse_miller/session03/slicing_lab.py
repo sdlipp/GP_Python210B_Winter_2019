ffff0351cccd10115fe564215e3eddd8aeb22ff2
UncleanlyCleric <junyatwin@gmail.com>
2019-01-29 11:08:03 -0800

    Right, slices.  Fixed every other.  Working on first/last four now.

--- students/jesse_miller/session03/slicing_lab.py
+++ students/jesse_miller/session03/slicing_lab.py
@@ -1,18 +1,21 @@
 #!/usr/local/bin/python3
 #This function swaps the first and last characters in the tuple.
 def exchange_first_last(seq):
-    seq = seq[-1:] + seq[1:-1] + seq[0:1]
-    return seq
+    result = seq[-1:] + seq[1:-1] + seq[0:1]
+    return result
 
 #This will take the string and remove every other character, including spaces.
 def every_other_removed(seq):
-    result = ""
-    for i in range(len(seq)):
-        if i % 2 == 0:
-            result = result + seq[i]
+#    result = ""
+#    for i in range(len(seq)):
+#        if i % 2 == 0:
+#            result = result + seq[i]
+    result = seq[::2]
+    print(result)
     return result
 
 def first_last_four(seq):
+
     return a_new_sequence
 
 def reversed(seq):
@@ -29,3 +32,6 @@ print("Test Passed - first and last numbers exchanged")
 
 assert every_other_removed(a_string) == "agia a tig htscoe"
 print("Test Passed - every other removed")
+
+#assert first_last_four(a_string) == "itar has strings that's c"
+#print("Test Passed - every other removed")
