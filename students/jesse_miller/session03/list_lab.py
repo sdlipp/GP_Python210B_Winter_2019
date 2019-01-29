0db0f48760810be6d1a9d605926caa0ed90a56b2
UncleanlyCleric <junyatwin@gmail.com>
2019-01-29 12:46:58 -0800

    Figured out appending and adding to the list

--- students/jesse_miller/session03/list_lab.py
+++ students/jesse_miller/session03/list_lab.py
@@ -5,7 +5,7 @@ fruits = ['Apples', 'Pears', 'Oranges', 'Peaches']
 print("Here is the current list of fruits:")
 print(fruits)
 #Here, we ask for a new fruit input.  There's no error correction, so the user
-#can input any string, but I suppose that'd be a possibliity unless we checked
+#can input any s, but I suppose that'd be a possibliity unless we checked
 #against a list of every possible fruit.
 n = str(input("Please enter a new fruit: "))
 #Here we're appending the user input to the list above.  If there's another way
@@ -13,14 +13,15 @@ n = str(input("Please enter a new fruit: "))
 fruits.append(n)
 print(fruits)
 #Getting user input for the number in the list to display.
-m = int(input("Please enter a number to show the fruit you want: "))
+n = int(input("Please enter a number to show the fruit you want: "))
 #The -1 is to compensate for the fact that arrays start with 0 and people don't
 #think that way.
-print(fruits[m-1])
-
-
-
+print(fruits[n-1])
+#Here is an example of adding a fruit to the list using addition.
+n = str(input("Please enter a new fruit: "))
+fruits = [n] + fruits
+print(fruits)
 #Here is an example of adding a fruit to the list using insert.
-o = str(input("Please enter a new fruit: "))
-fruits.insert(0,o)
+n = str(input("Please enter a new fruit: "))
+fruits.insert(0,n)
 print(fruits)
