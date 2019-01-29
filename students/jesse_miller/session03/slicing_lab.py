dd2e73bc7e8b4ad429abc496b67d189d7aa4768b
UncleanlyCleric <junyatwin@gmail.com>
2019-01-29 11:42:42 -0800

    Added one more number to make nine.  Slicing thirds will probably be easier like that.

--- students/jesse_miller/session03/slicing_lab.py
+++ students/jesse_miller/session03/slicing_lab.py
@@ -7,15 +7,15 @@ def exchange_first_last(seq):
     return result
 
 #This will take the string and remove every other character, including spaces.
+#The slice here starts at 0 and slices out every second character.
 def every_other_removed(seq):
     result = seq[::2]
     return result
-
-
+#This function slices out the first four, and four from the last characters
 def first_last_four(seq):
     result = seq[4:-4]
     return result
-
+#The :: operand reverses the output, starting at the last position of the tuple
 def reversed(seq):
     result = seq[::-1]
     return result
@@ -24,9 +24,9 @@ def first_last_middle(seq):
     return a_new_sequence
 
 a_string = "a guitar has strings that's close"
-a_tuple = (2, 54, 13, 12, 5, 32, 3, 82)
+a_tuple = (2, 54, 13, 12, 5, 32, 3, 82, 1)
 
-assert exchange_first_last(a_tuple) == (82, 54, 13, 12, 5, 32, 3, 2)
+assert exchange_first_last(a_tuple) == (1, 54, 13, 12, 5, 32, 3, 82, 2)
 print("Test Passed - first and last numbers exchanged")
 
 assert every_other_removed(a_string) == "agia a tig htscoe"
@@ -35,5 +35,5 @@ print("Test Passed - every other removed")
 assert first_last_four(a_string) == "itar has strings that's c"
 print("Test Passed - first and last four characters removed")
 
-assert reversed(a_tuple) == (82, 3, 32, 5, 12, 13, 54, 2)
+assert reversed(a_tuple) == (1, 82, 3, 32, 5, 12, 13, 54, 2)
 print("Test Passed - tuple reversed")
