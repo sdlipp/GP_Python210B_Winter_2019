7deaf11785511248d0dfa6ccd0107fabd377faf4
UncleanlyCleric <junyatwin@gmail.com>
2019-01-29 16:51:19 -0800

    Trying to work through my roadblocks, unsuccessfully

--- students/jesse_miller/session03/strformat_lab.py
+++ students/jesse_miller/session03/strformat_lab.py
@@ -1,4 +1,5 @@
 #!/usr/local/bin/python3
+#Task 1
 #Defining the tuple, just in case
 tuple = ( 2, 123.4567, 10000, 12345.67)
 
@@ -6,5 +7,15 @@ tuple = ( 2, 123.4567, 10000, 12345.67)
 #Apparently, the old fashioned way:
 print('File_00{0} :  {1:.2f}, {2:.2e}, {3:.2e}'.format(2, 123.4567, 10000, 12345.67))
 
+#Task 2
 #Using an f-string
 print(f"File_00{2} :  {123.4567:.2f}, {10000:.2e}, {12345.67:.2e}")
+
+#Task 3
+#reference example
+#print("the 3 numbers are: {:d}, {:d}, {:d}".format(1,2,3))
+n = len(tuple)
+def formatter(self):
+    form_string = "{:d}" * n
+    print(f"{}" * n)
+formatter(tuple)
