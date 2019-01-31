11c5fe085015c471f8c4b187ab6511271b4fcff6
UncleanlyCleric <junyatwin@gmail.com>
2019-01-30 17:39:29 -0800

    I can print the list, but I'm stuck on figuring out how to add the ints in the list.

--- students/jesse_miller/session03/mailroom.py
+++ students/jesse_miller/session03/mailroom.py
@@ -1,26 +1,26 @@
 #!/usr/local/bin/python3
 import sys
 
-donors = [("Robert Smith", 435.56, 125.23, 357.10),
-            ("JD Cronise", 123.12, 25.05, 75.22),
-            ("Chris Stapleton", 63.23, 243.87, 111.32),
-            ("Dave Grohl", 63.23, 422.87, 9432.01),
-            ("Randy Blythe", 223.50, 350.12, 8120.32),
-            ("Devin Townsand", 431.12, 342.92, 5412.45),
+donors = [["Robert", 435.56, 125.23, 357.10],
+            ["JD", 123.12],
+            ["Chris", 243.87, 111.32],
+            ["Dave", 63.23, 422.87, 9432.01],
+            ["Randy", 223.50, 8120.32],
+            ["Devin", 431.12, 342.92, 5412.45],
             ]
 
 prompt = "\n".join(("Welcome to mailroom 0.1!",
         "Please choose from below options:",
-        "1 - If you would like to send a Thank You.",
-        "2 - If you would like a report of donations totals.",
-        "3 - Exit.",
+        "send - If you would like to send a Thank You.",
+        "report - If you would like a report of donations totals.",
+        "exit - Exit.",
         ">>> "))
 
 def donor_list():
-#        print("{}".format(donor))
-#    mail_menu()
-#
-    print("\n".join(donors))
+    for i in range(len(donors)):
+        for j in range(len(donors[i])):
+            print(donors[i][j], end=' ')
+        print()
 
 def mail_menu():
     prompt = "\n".join(("Donor and Mail Database",
@@ -28,12 +28,13 @@ def mail_menu():
           "list - If you would like to see a list of donors.",
           "mail - If you would like to send a thank you.",
           "new - If you would like to add a new donor.",
-          "back - If you would like to return to the main menu."
+          "back - If you would like to return to the main menu.",
           ">>> "))
     while True:
         mail_input = input(prompt)
         if mail_input in ('list'):
-            print(donor_list)
+            donor_list()
+            print("")
         elif mail_input in ('name'):
             print(donor_list)
         elif mail_input in ('back'):
@@ -54,11 +55,11 @@ def main():
     while True:
         response = input(prompt)  # continuously collect user selection
         # now redirect to feature functions based on the user selection
-        if response == "1":
+        if response == "send":
             mail_menu()
-        elif response == "2":
+        elif response == "report":
             mail_menu()
-        elif response == "3":
+        elif response == "exit":
             goodbye()
         else:
             print("Not a valid option!")
