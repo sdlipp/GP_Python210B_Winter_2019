fae88f8bd6ef30221b7e60c469b2ef38eefcaf20
UncleanlyCleric <junyatwin@gmail.com>
2019-01-31 11:46:52 -0800

    Still working on the reporting section

--- students/jesse_miller/session03/mailroom.py
+++ students/jesse_miller/session03/mailroom.py
@@ -15,9 +15,7 @@ print("")
 PROMPT = "\n".join(("Welcome to mailroom 0.1!",
                     "",
                     "Please choose from below options:",
-                    "send - If you would like to send a Thank You.",
-                    "add - If you would like to add a donor.",
-                    "remove - If you would like to delete a donor",
+                    "mail - If you would like the mail menu.",
                     "report - If you would like a report of donations totals.",
                     "quit - Exit.",
                     ">>> "))
@@ -25,50 +23,22 @@ PROMPT = "\n".join(("Welcome to mailroom 0.1!",
 SEND_PROMPT = "\n".join(("Donor and Mail Database",
                          "",
                          "Please choose from below options:",
+                         "send - If you would like to send a thank you.",
                          "list - If you would like to see a list of DONORS.",
-                         "mail - If you would like to send a thank you.",
                          "back - If you would like to return to the main menu.",
                          ">>> "))
-
-def donor_list():
-    '''
-    This when done properly, will print the list of donor names
-    '''
-    print("-" * 15)
-    print("List of donors: ")
-    print("-" * 15)
-    for donor in DONORS:
-        print(donor)
-    print("")
-    main()
-
-#def donor_mail():
-
-def donor_add():
-    new_donor = str(input("Enter the name of the new donor: "))
-    new_don1 = float(input("Enter their first donation: "))
-    new_don2 = float(input("Enter the second donation: "))
-    new_don3 = float(input("Enter the third donation: "))
-    DONORS[new_donor] = new_don1, new_don2, new_don1
-    donor_list()
-
-def donor_del():
-    del_donor = str(input("Enter the name of the donor to remove: "))
-    del DONORS[del_donor]
-    donor_list()
 
 def mail_menu():
     '''
     This is the menu for the mail section
     '''
-
     while True:
         mail_input = input(SEND_PROMPT)
         if mail_input.lower() == 'list':
             print("")
             donor_list()
             print("")
-        if mail_input.lower() == 'mail':
+        if mail_input.lower() == 'send':
             print("")
             donor_list()
         if mail_input.lower() == 'back':
@@ -82,11 +52,10 @@ def report():
     '''
     This will be the donation report section
     '''
-    print("-" * 15)
-    print("Donation report: ")
-    print("-" * 15)
     for donor in DONORS:
-        print({k:sum(map(float, v)) for k, v in DONORS.items()})
+        name = donor
+        avg = len(DONORS) - 1
+        print(name, avg)
     print("")
     main()
 
@@ -97,27 +66,55 @@ def goodbye():
     print("Goodbye!")
     sys.exit()
 
+def donor_list():
+    '''
+    This when done properly, will print the list of donor names
+    '''
+    print("-" * 15)
+    print("List of donors: ")
+    print("-" * 15)
+    for donor in DONORS:
+        print(donor)
+    print("")
+    mail_menu()
+
+def donor_mail():
+    """
+    This section allows the user to mail a donor
+    """
+    mail_menu()
+
+def donor_add():
+    """
+    This section allows the user to add a donor
+    """
+    new_donor = str(input("Enter the name of the new donor: "))
+    new_don1 = float(input("Enter their first donation: "))
+    new_don2 = float(input("Enter the second donation (0 if there isn't one): "))
+    new_don3 = float(input("Enter the third donation (0 if there isn't one): "))
+    DONORS[new_donor] = new_don1, new_don2, new_don3
+    donor_list()
+
+def donor_del():
+    """
+    This section allows the user to delete a donor
+    """
+    del_donor = str(input("Enter the name of the donor to remove: "))
+    del DONORS[del_donor]
+    donor_list()
+
 def main():
     '''
     The man menu and the calls to other functions
     '''
     while True:
         response = input(PROMPT)
-        '''
-        Continuously collect user selection
-        '''
-        if response.lower() == "send":
+        if response.lower() == "mail":
             print("")
             mail_menu()
         if response.lower() == "report":
             print("")
             report()
-        if response.lower() == 'add':
-            print("")
-            donor_add()
-        if response.lower() == 'remove':
-            print("")
-            donor_del()
         if response.lower() == "quit":
             print("")
             goodbye()
