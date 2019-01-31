d15ab331f8b57e1f3262246ea59cc71f7fe14cbc
mjchang14 <mjchang14@gmail.com>
2019-01-31 00:08:34 -0800

    list_lab series 1, all but the last step

--- students/mjchang/session03/list_lab.py
+++ students/mjchang/session03/list_lab.py
@@ -9,6 +9,14 @@ fruits.append(response1)
 print(fruits)
 fruit_count = len(fruits)
 response2 = input("Pick a number between 1 and {}: ".format(fruit_count))
-num = int(response2) + 1
-name_fruit = 
-print("{} is a {}".format(num, name_fruit))
+num = int(response2)
+y = num - 1
+name_fruit = fruits[y]
+print("{} is {}".format(num, name_fruit))
+response3 = input("Add another fruit to the list: ")
+fruits = [response3] + fruits
+print(fruits)
+response4 = input("Give me one more fruit: ")
+fruits.insert(0, response4)
+print(fruits)
+
