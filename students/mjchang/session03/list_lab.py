3cf5f74e5613ba95d710f270a059cb8e736a3118
mjchang14 <mjchang14@gmail.com>
2019-01-31 18:50:41 -0800

    series 1 exercise complete

--- students/mjchang/session03/list_lab.py
+++ students/mjchang/session03/list_lab.py
@@ -5,6 +5,7 @@
 fruits = ['Apples', 'Pears', 'Oranges', 'Peaches']
 print(fruits)
 response1 = input("Please add another fruit to the list: ")
+response1 = response1.capitalize()
 fruits.append(response1)
 print(fruits)
 fruit_count = len(fruits)
@@ -14,9 +15,11 @@ y = num - 1
 name_fruit = fruits[y]
 print("{} is {}".format(num, name_fruit))
 response3 = input("Add another fruit to the list: ")
-fruits = [response3] + fruits
+fruits = [response3.capitalize()] + fruits
 print(fruits)
 response4 = input("Give me one more fruit: ")
-fruits.insert(0, response4)
-print(fruits)
-
+fruits.insert(0, response4.capitalize())
+print("The fruits that start with the letter P are:")
+for p_fruit in fruits:
+    if p_fruit[0] == 'P':
+        print(p_fruit)
