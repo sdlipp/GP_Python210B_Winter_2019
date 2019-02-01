baa9d283a248ae0b1398afd4942f0341e35165d6
UncleanlyCleric <junyatwin@gmail.com>
2019-02-01 10:27:55 -0800

    So far, everything works except for the mail part.

--- students/jesse_miller/session03/mailroom.py
+++ students/jesse_miller/session03/mailroom.py
@@ -4,7 +4,7 @@ Beginning of my mailroom implementation.
 """
 import sys
 
-DONORS = {"Robert Smith" : [435.56, 125.23, 357.10],
+donors = {"Robert Smith" : [435.56, 125.23, 357.10],
           "JD Cronise" : [123.12],
           "Chris Stapleton" : [243.87, 111.32],
           "Dave Lombardo" : [63.23, 422.87, 9432.01],
@@ -15,17 +15,17 @@ print("")
 PROMPT = "\n".join(("Welcome to mailroom 0.1!",
                     "",
                     "Please choose from below options:",
-                    "mail - If you would like the mail menu.",
+                    "mail   - If you would like the mail menu.",
                     "report - If you would like a report of donations totals.",
-                    "quit - Exit.",
+                    "quit   - Exit.",
                     ">>> "))
 
 SEND_PROMPT = "\n".join(("Donor and Mail Database",
                          "",
                          "Please choose from below options:",
-                         "send - If you would like to send a thank you.",
-                         "list - If you would like to see a list of donors.",
-                         "back - If you would like to return to the main menu.",
+                         "send   - If you would like to send a thank you.",
+                         "list   - If you would like to see a list of donors.",
+                         "back   - If you would like to return to the main menu.",
                          ">>> "))
 
 def mail_menu():
@@ -45,6 +45,7 @@ def mail_menu():
             print("")
             main()
         else:
+            print("")
             print("Not a valid option")
             print("")
 
@@ -52,14 +53,14 @@ def report():
     '''
     This will be the donation report section
     '''
-    summary= []
+    summary = []
     headers = ["Donor Name", "Total Given", "Times Donated", "Average Gift"]
     print()
     print("-"*80)
     print("{:17} | {:<20} | {:<15} | {:<19}".format(headers[0], headers[1], headers[2], headers[3]))
     print("-"*80)
 
-    for k,v in DONORS.items():
+    for k,v in donors.items():
         donor = k
         total = (sum(v))
         times = (len(v))
@@ -86,61 +87,86 @@ def donor_list():
     print("-" * 15)
     print("List of donors: ")
     print("-" * 15)
-    for donor in DONORS:
+    for donor in donors:
         print(donor)
     print("")
-    #mail_menu()
 
 def donor_mail():
     """
     This section allows the user to mail a donor
     """
+    global donor_send
+    donor_send = ""
     while True:
         donor_list()
         donor_send = str(input("Who would you like to mail: "))
-        for donor in DONORS:
+        for donor in donors:
             if donor_send == donor:
-                mail_send()
+                mail_menu()
             else:
-                new_donor = donor_send
-                d_num = int(input("How many donations were made: "))
-                if d_num == 1:
-                    new_don1 = float(input("Enter their first donation: "))
-                    DONORS[new_donor] = new_don1
-                if d_num == 2:
-                    new_don1 = float(input("Enter their first donation: "))
-                    new_don2 = float(input("Enter the second donation: "))
-                    DONORS[new_donor] = new_don1, new_don2
-                if d_num == 3:
-                    new_don1 = float(input("Enter their first donation: "))
-                    new_don2 = float(input("Enter the second donation: "))
-                    new_don3 = float(input("Enter the third donation: "))
-                    DONORS[new_donor] = new_don1, new_don2, new_don3
-                else:
-                    print("Please enter only 1, 2 or 3")
-                donor_list()
+                donor_add()
             mail_menu()
 
-def mail_send():
-    """
-    This is the meat of the send process
-    """
-    mail_menu()
+def donor_add():
+    if donor_send not in donors:
+        donors[donor_send] = []
+    d_num = int(input("How many donations were made: "))
+    while d_num > 0:
+        new_don = float(input("Enter their first donation: "))
+        """
+        This should append the donation to the dict entry
+        """
+        donors[donor_send].append(new_don)
+        """
+        Decreases the value to d_num to end the loop
+        """
+        d_num -= 1
+    donor_list()
 
 def donor_del():
     """
     This section allows the user to delete a donor
     """
     del_donor = str(input("Enter the name of the donor to remove: "))
-    del DONORS[del_donor]
+    del donors[del_donor]
     donor_list()
+
+#def mail_send():
+#    """
+#    This is the meat of the send process
+#    """
+#    global donor_email
+#    global donor_total
+#    global donor_avg
+#    donor_email = ""
+#    donor_total = ""
+#    donor_avg = ""
+
+#mail = ("Hello {}, \n"
+#        "\n"
+#        "We are writing to thank you for you generous donoation\n"
+#        "to our foundation.  Your contributions for the year \n"
+#        "total ${} in {} disbursements. \n"
+#        "\n"
+#        "Again, the foundation thanks you for your support, \n"
+#        "and we hope to remain in contact with you in this new \n"
+#        "year.\n"
+#        "\n"
+#        "Sincerely, \n"
+#        "Ecumenical Slobs LLC \n".format(donor_email,donor_total,donor_avg))
+
 
 def main():
     '''
     The man menu and the calls to other functions
     '''
+    valid_input = ("mail","report","quit")
     while True:
         response = input(PROMPT)
+        if response not in valid_input:
+            print("")
+            print("Not a valid option!")
+            print("")
         if response.lower() == "mail":
             print("")
             mail_menu()
@@ -150,10 +176,7 @@ def main():
         if response.lower() == "quit":
             print("")
             goodbye()
-        else:
-            print("")
-            print("Not a valid option!")
-            print("")
+
 
 if __name__ == "__main__":
     main()
