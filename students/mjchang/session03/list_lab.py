4fa87e58f0def2cf4f812589ae0c15c87b3b5a65
mjchang14 <mjchang14@gmail.com>
2019-01-31 19:55:12 -0800

    lower case fruit name in series 3 exercise

--- students/mjchang/session03/list_lab.py
+++ students/mjchang/session03/list_lab.py
@@ -4,46 +4,48 @@
 
 fruits = ['Apples', 'Pears', 'Oranges', 'Peaches']
 print(fruits)
-response1 = input("Please add another fruit to the list: ")
-response1 = response1.capitalize()
-fruits.append(response1)
-print(fruits)
-fruit_count = len(fruits)
-response2 = input("Pick a number between 1 and {}: ".format(fruit_count))
-num = int(response2)
-y = num - 1
-name_fruit = fruits[y]
-print("{} is {}".format(num, name_fruit))
-response3 = input("Add another fruit to the list: ")
-fruits = [response3.capitalize()] + fruits
-print(fruits)
-response4 = input("Give me one more fruit: ")
-fruits.insert(0, response4.capitalize())
-print(fruits)
-print("The fruits that start with the letter P are:")
-for p_fruit in fruits:
-    if p_fruit[0] == 'P':
-        print(p_fruit)
+# response1 = input("Please add another fruit to the list: ")
+# response1 = response1.capitalize()
+# fruits.append(response1)
+# print(fruits)
+# fruit_count = len(fruits)
+# response2 = input("Pick a number between 1 and {}: ".format(fruit_count))
+# num = int(response2)
+# y = num - 1
+# name_fruit = fruits[y]
+# print("{} is {}".format(num, name_fruit))
+# response3 = input("Add another fruit to the list: ")
+# fruits = [response3.capitalize()] + fruits
+# print(fruits)
+# response4 = input("Give me one more fruit: ")
+# fruits.insert(0, response4.capitalize())
+# print(fruits)
+# print("The fruits that start with the letter P are:")
+# for p_fruit in fruits:
+#     if p_fruit[0] == 'P':
+#         print(p_fruit)
 
 
 # series 2
 
-fruits2 = fruits
-print(fruits2)
-print("The last fruit will be removed")
-fruits2.pop()
-print(fruits2)
-bye_fruit = input("Pick one more fruit from the list to remove: ")
-fruits2.remove(bye_fruit)
-print(fruits2)
+# fruits2 = fruits
+# print(fruits2)
+# print("The last fruit will be removed")
+# fruits2.pop()
+# print(fruits2)
+# bye_fruit = input("Pick one more fruit from the list to remove: ")
+# fruits2.remove(bye_fruit)
+# print(fruits2)
 
 
 # series 3
 
 fruits3 = fruits
+for low_fruit in fruits3:
+    low_fruit = low_fruit.lower()
+tasty = input("Do you like {}? ".format(low_fruit)) 
 
-
-
+    
 
 
 
