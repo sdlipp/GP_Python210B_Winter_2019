8bbfbb68a2487214ed6308f96b3b06fb4c4be536
mjchang14 <mjchang14@gmail.com>
2019-02-02 15:33:57 -0800

    finished series 3

--- students/mjchang/session03/list_lab.py
+++ students/mjchang/session03/list_lab.py
@@ -11,6 +11,8 @@ print(fruits)
 fruit_count = len(fruits)
 response2 = input("Pick a number between 1 and {}: ".format(fruit_count))
 num = int(response2)
+if num > 5:
+    num = 5 #in case a number larger than 5 is entered, default to 5
 y = num - 1
 name_fruit = fruits[y]
 print("{} is {}".format(num, name_fruit))
@@ -34,23 +36,26 @@ print("The last fruit will be removed")
 fruits2.pop()
 print(fruits2)
 bye_fruit = input("Pick one more fruit from the list to remove: ")
-fruits2.remove(bye_fruit)
+fruits2.remove(bye_fruit.capitalize())
 print(fruits2)
 
 
 # series 3
 
 fruits3 = fruits
-fruits3 = [n.lower() for n in fruits3]
-for n in fruits3:
-    tasty = input("Do you like {}? ".format(n))
-    if tasty == 'no':  
-        fruits3.remove(n)
-    elif tasty == "yes":
-        pass
-    else:
-        print("Please enter yes or no")
-print("The fruits you like are {}:".format(fruits3))
+copy_fruits3 = fruits3[:]
+fruits3 = [favorites.lower() for favorites in fruits3]
+copy_fruits3 = [favorites.lower() for favorites in copy_fruits3]
+for favorites in fruits3:
+    tasty = input("Do you like {}? ".format(favorites))
+    tasty = tasty.lower() # makes the list lowercase
+    
+    while tasty != "no" and tasty != "yes":
+        tasty = input("Please enter yes or no: ")
+    if tasty == "no":
+        copy_fruits3.remove(favorites)   
+        
+print("The fruits you like are {}:".format(copy_fruits3))
  
 
 
