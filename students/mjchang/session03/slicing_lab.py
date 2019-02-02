3fb76df8c35f8647718c47094c34186ff394be28
mjchang14 <mjchang14@gmail.com>
2019-02-01 23:50:15 -0800

    adding slicing lab with tests

--- students/mjchang/session03/slicing_lab.py
+++ students/mjchang/session03/slicing_lab.py
@@ -32,3 +32,24 @@ tuple_test = (23, 7, 41, 96, 5, 8, 1000, 38, 17, 31, 14, 25, 2, 12, 4, 87, 15)
 assert four_on_ends(str_test) == "igti t"
 assert four_on_ends(tuple_test) == (5, 1000, 17, 14, 2)
 
+# reverse the elements with just slicing
+def element_rev(seq):
+    return seq[::-1]
+
+str_test = "testing this string"
+tuple_test = (23, 7, 41, 96, 5, 8)
+
+assert element_rev(str_test) == "gnirts siht gnitset"
+assert element_rev(tuple_test) == (8, 5, 96, 41, 7, 23)
+
+# last third, first third, the middle third, in that order
+def thirds(seq):
+    one_third = int(len(seq)/3)
+    return seq[-one_third:]+seq[:one_third+len(seq)%3]+seq[one_third+len(seq)%3:-one_third]
+
+str_test = "testing this string"
+tuple_test = (23, 7, 41, 96, 5, 8, 1000, 38, 17, 31, 14, 25, 2, 12, 4, 87, 15)    
+
+assert thirds(str_test) == "stringtesting this "
+assert thirds(tuple_test) == (2, 12, 4, 87, 15, 23, 7, 41, 96, 5, 8, 1000, 38, 17, 31, 14, 25)
+
