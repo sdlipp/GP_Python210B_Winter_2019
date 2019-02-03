4eb4d418c16387a9cfecb17ff546d317224fcec3
Rockwell70 <rockwellshabani@gmail.com>
2019-02-02 17:29:43 -0800

    Shabani repo

--- students/jeff_shabani/session03/list_lab.py
+++ students/jeff_shabani/session03/list_lab.py
@@ -1,51 +1,51 @@
 #!/usr/bin/env python3
 
 
-FRUIT_LIST = ['Apples', 'Pears', 'Oranges', 'Peaches']
+fruit_list = ['Apples', 'Pears', 'Oranges', 'Peaches']
 
 def main():
 
-    def series_one(fruit_list):
-        print(fruit_list)
+    def series_one(f_list):
+        print(f_list)
         answer = input('What fruit to you want to add?')
-        fruit_list.append(answer)
-        print(fruit_list)
-        index_answer = int(input(f'Please enter a number between 1 and {len(fruit_list)}'))
-        print(fruit_list[index_answer - 1])
+        f_list.append(answer)
+        print(f_list)
+        index_answer = int(input(f'Please enter a number between 1 and {len(f_list)}'))
+        print(f_list[index_answer - 1])
         # add new fruit begging of list
-        fruit_list = ['Pineapple'] + fruit_list
-        print(fruit_list)
-        fruit_list.insert(0, 'Tomato')
-        print(fruit_list)
-        for i in fruit_list:
+        f_list = ['Pineapple'] + f_list
+        print(f_list)
+        f_list.insert(0, 'Tomato')
+        print(f_list)
+        for i in f_list:
             if i[0] == 'P':
                 print(i)
 
-    series_one(FRUIT_LIST)
+    series_one(fruit_list)
 
 
-    def series_two(fruit_list):
+    def series_two(f_list):
         # includes bonus
-        print(fruit_list)
-        fruit_list.pop()
-        print(fruit_list)
+        print(f_list)
+        f_list.pop()
+        print(f_list)
         delete_answer = input('Select a fruit to delete')
-        fruit_list = fruit_list * 2
-        while delete_answer not in fruit_list:
+        f_list = f_list * 2
+        while delete_answer not in f_list:
             delete_answer = input('Please select another fruit')
         else:
-            for i in fruit_list:
+            for i in f_list:
                 if i == delete_answer:
-                    fruit_list.remove(i)
-        print(fruit_list)
+                    f_list.remove(i)
+        print(f_list)
 
-    series_two(FRUIT_LIST)
+    series_two(fruit_list)
 
 
-    def series_three(fruit_list):
+    def series_three(f_list):
         acceptable_answers = {'yes', 'no'}
         result = []
-        for i in fruit_list:
+        for i in f_list:
             answer = input(f'Do you like {i}?')
             while answer.lower() not in acceptable_answers:
                 answer = input(f'Please answer yes or no: Do you like {i}?')
@@ -54,17 +54,17 @@ def main():
                 result.append(i)
         print(result)
 
-    series_three(FRUIT_LIST)
+    series_three(fruit_list)
 
-    def series_four(fruit_list):
+    def series_four(f_list):
         result = []
-        for i in fruit_list:
+        for i in f_list:
             result.append(i[::-1])
-        fruit_list.pop()
+        f_list.pop()
         print(f'Copy of original with letters of each item reversed: {result}')
-        print(f'Original with last item removed: {fruit_list}')
+        print(f'Original with last item removed: {f_list}')
 
-    series_four(FRUIT_LIST)
+    series_four(fruit_list)
 
 if __name__ == '__main__':
     main()
