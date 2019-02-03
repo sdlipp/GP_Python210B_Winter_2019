9cbbe08c2c74d0bbab8fa5b4dff23fa07220c40d
Jeremy <jereamon@gmail.com>
2019-02-02 16:55:22 -0800

    Mailroom Pt 1 is complete. I'll have to remember to commit more. Didn't even realize I hand't the whole time I was working on that.

--- students/jeremy_m/lesson03_exercises/string_formatting_lab.py
+++ students/jeremy_m/lesson03_exercises/string_formatting_lab.py
@@ -12,19 +12,21 @@ print('file_{:03d} : {:06.2f}, {:.2E}, {:.2E}'.format(*first_tup))
 
 # TASK 2
 print('\nTask Two:')
-print(f'file_{first_tup[0]:03d} : {first_tup[1]:06.2f}, {first_tup[2]:.2E}, {first_tup[3]:.2E}')
-
+print(
+    f'file_{first_tup[0]:03d} : {first_tup[1]:06.2f}, {first_tup[2]:.2E}, {first_tup[3]:.2E}')
 
 
 # TASK 3
 print("\nTask Three:")
+
+
 def formatter(seq):
     l = len(seq)
     return ("the {} numbers are: " + ", ".join(['{}'] * l)).format(l, *seq)
 
+
 print(formatter(first_tup))
 print(formatter((1, 4, 5, 6, 7, 3, 2, 1)))
-
 
 
 # TASK 4
@@ -33,22 +35,19 @@ task_four_tup = (4, 30, 2017, 2, 27)
 print(sorted(task_four_tup))
 
 
-
 # TASK 5
 print("\nTask Five:")
 task_five_list = ['oranges', 1.3, 'lemons', 1.1]
-print(f'The weight of an {task_five_list[0][:-1]} is {task_five_list[1]} and the weight of a {task_five_list[2][:-1]} is {task_five_list[3]}')
-print(f'The weight of an {task_five_list[0][:-1].upper()} is {task_five_list[1] * 1.2} and the weight of a {task_five_list[2][:-1].upper()} is {task_five_list[3] * 1.2}')
-
+print(
+    f'The weight of an {task_five_list[0][:-1]} is {task_five_list[1]} and the weight of a {task_five_list[2][:-1]} is {task_five_list[3]}')
+print(
+    f'The weight of an {task_five_list[0][:-1].upper()} is {task_five_list[1] * 1.2} and the weight of a {task_five_list[2][:-1].upper()} is {task_five_list[3] * 1.2}')
 
 
 # TASK 6
 print("\nTask Six:")
-task_six_tup = (['Pretzel', '12', '$5.75'],
-                ['Tuna Fish', '62', '$62.99'],
-                ['PB&J', '4', '$126.33'],
-                ['Freeze Dried Tomato', '664', '$1242.96']
-                )
+task_six_tup = (['Pretzel', '12', '$5.75'], ['Tuna Fish', '62', '$62.99'], [
+                'PB&J', '4', '$126.33'], ['Freeze Dried Tomato', '664', '$1242.96'])
 
 days_old = 'Days Old'
 for task in task_six_tup:
