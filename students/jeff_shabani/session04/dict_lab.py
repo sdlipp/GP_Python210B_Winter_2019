a5722f78aa90d8b1c072949dc4a9bdf2b9a5a568
Rockwell70 <rockwellshabani@gmail.com>
2019-02-03 10:15:07 -0800

    Shabani repo

--- students/jeff_shabani/session04/dict_lab.py
+++ students/jeff_shabani/session04/dict_lab.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python3
 
+from copy import deepcopy
+
 first = {'name': 'Chris',
          'city': 'Seattle',
          'cake': 'Chocolate'}
-
 
 def show_dict_items(any_dictionary):
     for k, v in any_dictionary.items():
@@ -24,17 +25,39 @@ def show_dict_values(any_dictionary):
 
 
 def dictionaries_one(first_dict):
+
+    #make a copy of the origingal dictionary
+    first_dict = deepcopy(first_dict)
+
     show_dict_items(first_dict)
     first_dict.pop('cake')
     print('After popping "cake":')
+
     show_dict_items(first_dict)
     first_dict['fruit'] = 'Mango'
     print('Dict keys with fruit added:')
+
     show_dict_keys(first_dict)
     print('Dict values with fruit added:')
+
     show_dict_values(first_dict)
     print(f'Cake is a key in the dictionary: {"cake" in first_dict}')
     print(f'Cake is a value in the dictionary: {"Mango" in first_dict.values()}')
 
 
-dictionaries_one(first)
+def dictionaries_two(a_dictionary):
+
+    #make a copy of the original dictionary
+    a_dictionary = deepcopy(a_dictionary)
+
+    new_dictionary = dict()
+
+    for key, value in a_dictionary.items():
+        new_dictionary[key] = value.lower().count('t')
+    show_dict_items(new_dictionary)
+
+
+if __name__ == '__main__':
+
+    dictionaries_one(first)
+    dictionaries_two(first)
