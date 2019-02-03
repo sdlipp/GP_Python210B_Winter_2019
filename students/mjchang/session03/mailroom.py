8578f213f4e4cb725a8e77ace2f7b36652235adb
mjchang14 <mjchang14@gmail.com>
2019-02-02 18:54:32 -0800

    almost complete except for existing donors

--- students/mjchang/session03/mailroom.py
+++ students/mjchang/session03/mailroom.py
@@ -6,18 +6,13 @@ donor_db = [("Eliza Sommers",[4000, 250, 70]),
 ("Paulina Rodriguez",[50000]), 
 ("Jacob Todd",[75, 80])]
 
-user_selection = input("""
-Please enter a number from the following options:
-    1 - Send a Thank You
-    2 - Create a Report
-    3 - Quit
-""")
-
-# def donor_search(name):
-#     for donor in donor_db:
-#         if name.lower() == donor[0].lower():
-#             return donor
-#     return None        
+def home():
+    print("""
+    Please enter a number from the following options:
+        1 - Send a Thank You
+        2 - Create a Report
+        3 - Quit
+    """)       
 
 def thank_you(): # adding a new vendor
     d_list = []
@@ -25,29 +20,28 @@ def thank_you(): # adding a new vendor
         d_list.append(donor[0].lower())
 
     while True:
-        donor_name = input("Please enter a full name (or 'list' for a list of current donors): ")
-        donor_name = donor_name.title() #keeps capitalization format the same
-        if donor_name.lower() == "list":
+        name = input("Please enter a full name (or 'list' for a list of current donors): ")
+        name = name.title() #keeps capitalization format the same
+        if name.lower() == "list":
             for donor in donor_db: # print list of donor names
                 print(donor[0]) 
-        elif donor_name.lower() not in d_list: #adding new donor
-            print("Adding {} to the donor list".format(donor_name))
-            donation = input("Enter the donation amount from {}: ".format(donor_name)) #adding donation from new donor
+        elif name.lower() not in d_list: #adding new donor
+            print("Adding {} to the donor list".format(name))
+            donation = input("Enter the donation amount from {}: ".format(name)) #adding donation from new donor
             donation = float(donation) #converting to a float
-            donor_db.append((donor_name, [donation]))
-            print(donor_db) # use this for testing only
+            donor_db.append((name, [donation]))
+            # print(donor_db) # use this for testing only
             break
-        elif donor_name.lower() in d_list:
+        elif name.lower() in d_list:
 
-            
-
-
-
-
-def ty_letter()
+            donation = input("Enter the new donation amount: ") 
+            donation = float(donation) #convert to a float
+            # donor_db[] = donor_db + donation
 
 
-
+# send the thank you letter
+    print("\n \n Generating the letter for {}\n \n".format(name))
+    print("Dear {}, \n On behalf of all of us, we thank your for your generous donation of ${:10.2f}. \n You have helped make a big impact on the community!".format(name, donation))
 
 
 # set up for donor report
@@ -70,22 +64,38 @@ def donor_report():
     print("-"*79) #print the dashed line
     for data in spreadsheet:    
         print("{:<30}  ${:12.2f}   {:>15}  ${:12.2f}".format(data[0], data[1], data[2], data[3]))   
+    home()
 
 
 
 def quit_program():
-    
-    sys.exit()
+    SystemExit
 
 
-if __name__ == "__main__":
+def main():    
     while True:
-        response = input(user_selection)
+        response = ''
+        response = input("""
+    Please enter a number from the following options:
+        1 - Send a Thank You
+        2 - Create a Report
+        3 - Quit
+    """)
         if response == "1":
             thank_you()
         elif response == "2":
             donor_report()
         elif response == "3":
             quit_program()
+            break
         else:
-            print("Please select from the available options")                    
+            print("Please select from the available options")   
+
+if __name__ == "__main__": 
+
+    donor_db = [("Eliza Sommers",[4000, 250, 70]),
+                ("Tao Chien",[350, 1000, 225]), 
+                ("Joaquin Andieta",[100, 25]), 
+                ("Paulina Rodriguez",[50000]), 
+                ("Jacob Todd",[75, 80])]   
+    main()             
