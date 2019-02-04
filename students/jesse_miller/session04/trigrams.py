d4b57a8115f6eb93186f3265bef023afbcb26ce5
UncleanlyCleric <junyatwin@gmail.com>
2019-02-04 15:46:08 -0800

    Yea, starting over, I was going down a rabbit hole, I know it.

--- students/jesse_miller/session04/trigrams.py
+++ students/jesse_miller/session04/trigrams.py
@@ -2,18 +2,9 @@
 """
 Trigrams attempt
 """
+import strings
+
 with open("sherlock_small.txt", "r+") as f:
-#    for str1 in file.readlines():
-    lines = f.readlines()
-sherlock = {}
-current_key = None
-for line in lines:
-    line = line.strip()
-    if not line:
-        current_key = None
-    elif not current_key:
-        current_key = line
-        sherlock[current_key] = []
-    else:
-        sherlock[current_key].append(line)
-print(sherlock)
+"""
+I am so overthinking this
+"""
