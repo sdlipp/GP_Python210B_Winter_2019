35df43611a16e3888c71e5fc1d0103a50d7911d6
UncleanlyCleric <junyatwin@gmail.com>
2019-02-05 18:03:14 -0800

    Nope, back to the drawing board.

--- students/jesse_miller/session04/trigrams.py
+++ students/jesse_miller/session04/trigrams.py
@@ -3,7 +3,6 @@
 Trigrams attempt
 """
 import string
-import codecs
 import random
 
 input_ = []
@@ -13,7 +12,7 @@ def open_file():
     I was so overthinking this, I think I've got it now however.  This opens
     the file and throws it into a list.
     """
-    with open('sherlock.txt', 'r') as f:
+    with open('sherlock_small.txt', 'r') as f:
         sherlock_input = f.read()
     cleanString(sherlock_input)
 
@@ -22,6 +21,7 @@ def cleanString(sherlock_input):
     This should strip out everything I don't want, punctuation wise, from the
     file
     """
+    sherlock_output = ""
     newstring = sherlock_input
     newstring = newstring.replace("!","")
     newstring = newstring.replace("@","")
@@ -52,7 +52,7 @@ def cleanString(sherlock_input):
     newstring = newstring.replace("\\","")
     newstring = newstring.replace("/","")
     return sherlock_output
-trigram(sherlock_output)
+
 
 
 def trigram(sherlock_output):
@@ -60,6 +60,7 @@ def trigram(sherlock_output):
     This should math and map out the trigram, at least, right now I think it
     will.
     """
+    swapped = ""
     for i in range(len(sherlock_output) - 2):
         trigram_input = tuple(sherlock_output[i:i + 2])
         trigram_fold = sherlock_output[i + 2]
@@ -73,20 +74,23 @@ def create_output(swapped, trigram_input):
     This should take the trigram mapping and use it against the text of the
     story.  At least, I hope.
     """
+    final_output = ""
     random_number = random.randrange(0, len(swapped))
     for i, trigram_input in enumerate(trigram):
         if i == random_number:
-            start_string = trigram_ibput
+            start_string = trigram_input
             break
     output_list = list(start_string) + trigram[start_string]
     return final_output
     print_output(final_output)
 
 def print_output(final_output):
+    print(final_output)
 
 
 
 def main():
+    open_file()
 
 if __name__ == "__main__":
     main()
