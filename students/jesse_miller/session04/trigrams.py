0971ec4daf22d8e34ebd79273190dd15e0e33e6d
UncleanlyCleric <junyatwin@gmail.com>
2019-02-06 11:24:59 -0800

    Not quite working how I want.  Still trying things.

--- students/jesse_miller/session04/trigrams.py
+++ students/jesse_miller/session04/trigrams.py
@@ -9,35 +9,42 @@ import string
 #import itertools
 
 
-def open_file():
+def parse_file():
     """
     I was so overthinking this, I think I've got it now however.  This opens
-    the file and throws it into a list, as well as strips punctuation.  Thanks
-    for the help Luis!
+    the file and throws it into a list, strips punctuation and creates the main
+    dictionary.
+    Thanks for the help Luis!
     """
+    temp_dict = {}
     sherlock_dict = {}
     translator = str.maketrans('', '', string.punctuation)
     sherlock_input = set(line.strip() for line in open('sherlock_small.txt'))
+
     for i in sherlock_input:
         i = i.translate(translator).split(' ')
-        print(i)
-#    for w in range(len(i)):
-#        sherlock_dict.append({get_var_name(key): key[l] for key in keys})
-#    print("")
-#    print(sherlock_dict)
+        temp_dict = dict(i[l:l+2] for l in range(0, len(i) - 2))
+        sherlock_dict = dict(list(sherlock_dict.items()) + \
+        list(temp_dict.items()))
+    create_tuples(sherlock_dict)
 
 
-# for item in i:
-#    sherlock_dict = dict(itertools.zip_longest(*[iter(i)] * 2, fillvalue=""))
-# print(sherlock_dict)
-# munge_file(sherlock_input)
+def create_tuples(sherlock_dict):
+    """
+    This should hopefully create the tuples needed for generating the trigrams
+    """
+    sherlock_tupled = []
+    for i in sherlock_dict:
+        k = (i, sherlock_dict[i])
+        sherlock_tupled.append(k)
+    print(list)
 
 
 def main():
     """
     Main run list
     """
-    open_file()
+    parse_file()
 
 
 if __name__ == "__main__":
