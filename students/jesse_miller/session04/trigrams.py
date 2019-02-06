f6194ce72bb4d00a5baa6b7aefe7c5eaaac199ce
UncleanlyCleric <junyatwin@gmail.com>
2019-02-05 16:53:00 -0800

    Still plodding along, it's testing okay in ipython though

--- students/jesse_miller/session04/trigrams.py
+++ students/jesse_miller/session04/trigrams.py
@@ -54,13 +54,38 @@ def cleanString(sherlock_input):
     return sherlock_output
 trigram(sherlock_output)
 
+
 def trigram(sherlock_output):
     """
-    This should math and map out the trigram
+    This should math and map out the trigram, at least, right now I think it
+    will.
     """
     for i in range(len(sherlock_output) - 2):
         trigram_input = tuple(sherlock_output[i:i + 2])
         trigram_fold = sherlock_output[i + 2]
+        trigram.setdefault(trigram_input, []).append(trigram_fold)
+    return swapped
+    create_output(swapped, trigram_input)
+
+
+def create_output(swapped, trigram_input):
+    """
+    This should take the trigram mapping and use it against the text of the
+    story.  At least, I hope.
+    """
+    random_number = random.randrange(0, len(swapped))
+    for i, trigram_input in enumerate(trigram):
+        if i == random_number:
+            start_string = trigram_ibput
+            break
+    output_list = list(start_string) + trigram[start_string]
+    return final_output
+    print_output(final_output)
+
+def print_output(final_output):
+
+
+
 def main():
 
 if __name__ == "__main__":
