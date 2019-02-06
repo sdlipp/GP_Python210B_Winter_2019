719ccc70f5719d7b1c47842d572cb6132c76e5f8
UncleanlyCleric <junyatwin@gmail.com>
2019-02-05 20:47:38 -0800

    At least I have the file reading in a better manner.

--- students/jesse_miller/session04/trigrams.py
+++ students/jesse_miller/session04/trigrams.py
@@ -11,34 +11,19 @@ import re
 #def open_file():
 """
 I was so overthinking this, I think I've got it now however.  This opens
-the file and throws it into a list.
-    """
-    #with open('sherlock_small.txt', 'r') as f:
-        #sherlock_input = f.read()
+the file and throws it into a list, as well as strips punctuation.
+"""
 sherlock_input = set(line.strip() for line in open('sherlock_small.txt'))
-sherlock_output = None
-#    cleanString(sherlock_input)
-#print(sherlock_input)
 for i in sherlock_input:
-    sherlock_input = i.rstrip().translate(str.maketrans(' ', ' ', string.punctuation))
+    sherlock_input = i.rstrip().translate(str.maketrans(' ', ' ',\
+    string.punctuation))
     print(sherlock_input)
-    sherlock_output = set.add(sherlock_input)
-print(sherlock_input)
-#def cleanString(sherlock_input):
-"""
-This should strip out everything I don't want, punctuation wise, from the
-file
-"""
-
-
-#def trigram(sherlock_output):
+#munge_file(sherlock_input)
 
 
 
-#def create_output(swapped, trigram_input):
 
 
-#def print_output(final_output):
 
 
 
