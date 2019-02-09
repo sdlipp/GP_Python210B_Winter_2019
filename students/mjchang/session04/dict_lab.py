371786435a6f74e6ff929331dac045a69b0e9983
mjchang14 <mjchang14@gmail.com>
2019-02-08 16:16:26 -0800

    adding set exercises

--- students/mjchang/session04/dict_lab.py
+++ students/mjchang/session04/dict_lab.py
@@ -38,3 +38,48 @@ for k, v in d2.items():
 print(d2)    
 
 
+#Sets
+
+#Set 1 - create sets s2, s3, s4 that contain numbers 0-20, divisible by 2, 3, and 4
+
+s2 = set()
+s3 = set()
+s4 = set()
+
+for x in range(21):
+    if not x % 2:
+        s2.add(x)
+    if not x % 3:    
+        s3.add(x)
+    if not x % 4:   
+        s4.add(x) 
+print(s2)
+print(s3)
+print(s4)
+
+#display if s3 is a subset of s2 (False)
+
+print(s3.issubset(s2))
+
+#display if s4 is a subset of s2 (True)
+
+print(s4.issubset(s2))
+
+
+#Set 2 - create a set with the letters in 'Python' and add 'i'
+
+p_set = set()
+for p in 'Python':
+    p_set.add(p.lower())
+p_set.add('i')    
+
+
+#create a frozen set with the letters in 'marathon'
+f_set = frozenset('marathon')
+
+#display the union and intersection of the two sets
+
+print("Union: ", p_set.union(f_set))
+print("Intersection: ", p_set.intersection(f_set))
+  
+
