c6adbfb463c589331b6dad518b5a9a98fb5d9cd8
UncleanlyCleric <junyatwin@gmail.com>
2019-02-09 11:19:42 -0800

    Trigrams updated again, requesting feedback.  Still not grasping this one, and I feel terrible about it.

--- students/jesse_miller/session04/trigrams.py
+++ students/jesse_miller/session04/trigrams.py
@@ -4,8 +4,7 @@ Trigrams attempt
 """
 import string
 import random
-import csv
-import re
+import sys
 
 def parse_file():
     """
@@ -14,13 +13,33 @@ def parse_file():
     dictionary.
     Thanks for the help Luis!
     """
-    translator = str.maketrans('', '', string.punctuation)
-    with open('sherlock_small.txt') as input_file:
+    startline = 'Produced by an anonymous Project Gutenberg'\
+                'volunteer and Jose Menendez'
+    endline = 'End of the Project Gutenberg EBook'
+
+    with open('sherlock.txt') as input_file:
         input_file = [line.rstrip('\n') for line in input_file]
         full_list = []
+        start = 0
         for line in input_file:
-            full_list.extend(line.translate(translator).split(' '))
+            if line[:len(startline)] == startline:
+                start = 1
+                continue
+            if not start:
+                input_file = [line.rstrip('\n') for line in input_file]
+            if line[:len(endline)] == endline:
+                break
+    return full_list
 
+def make_words(input_file):
+    translator = str.maketrans('', '', string.punctuation)
+    full_list = []
+    for line in input_file:
+        full_list.extend(line.translate(translator).split(' '))
+    return full_list
+
+
+def build_trigram(full_list):
     sherlock_dict = {}
     for i in range(0, len(full_list) - 2):
         trigram_key = (full_list[i], full_list[i+1])
@@ -29,20 +48,27 @@ def parse_file():
             sherlock_dict[trigram_key].append(trigram_value)
         else:
             sherlock_dict[trigram_key] = [trigram_value]
-
-    for key in sherlock_dict:
-        print(f'{key}) = {sherlock_dict[key]}')
-
     return sherlock_dict
 
+def build_text(sherlock_dict):
     pass
 
 def main():
     """
     Main run list
     """
-    parse_file()
+    try:
+        filename = sys.argv[1]
+    except IndexError:
+        print("You must pass in a filename")
+        sys.exit(1)
 
+    in_data = parse_file()
+    words = make_words(input_file)
+    word_pairs = build_trigram(full_list)
+    new_text = build_text(sherlock_dict)
+
+    print(new_text)
 
 if __name__ == "__main__":
     main()
