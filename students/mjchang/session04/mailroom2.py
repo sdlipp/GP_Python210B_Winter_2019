f100f0fc8000f98e9e5949f70ea154df250f2a5f
mjchang14 <mjchang14@gmail.com>
2019-02-10 23:43:10 -0800

    defined update donor

--- students/mjchang/session04/mailroom2.py
+++ students/mjchang/session04/mailroom2.py
@@ -17,38 +17,44 @@ def home():
     """)       
 
 
-def show_donors(): #creating a list of donors to be displayed if "list" option selected
-    d_list = []
-    for donor in donor_db.values():
-        d_list.append(donor[0])
-    print("\n".join(d_list))    
-
-def thank_you_one(): # adding a new vendor
+def thank_you_one():
     while True:
         name = input("Please enter a full name (or 'list' for a list of current donors): ")
         name = name.title() #keeps capitalization format the same
         if name.lower() == "list":
-            show_donors()
-                
+            show_donors() 
         elif name.lower() not in d_list: #adding new donor
-            print("Adding {} to the donor list".format(name))
-            donation = input("Enter the donation amount from {}: ".format(name)) #adding donation from new donor
-            donation = float(donation) #converting to a float
-            donor_db[name] = donation #adding new donor and donation to the list
-            break
+            add_donor()
         elif name.lower() in d_list:
-            name_index = d_list.index(name.lower())
-            donation = input("Enter the new donation amount: ")
-            donation = float(donation)
-            donor_db[name_index][1].append(donation)
-            # print(donor_db) #for testing
-            print("\n \n The {} donation from {} was added".format(donation, name))
-            break
-        break       
-
-# send the thank you letter
+            update_donor()
+        break  
+        # send the thank you letter
     print("\n \n Generating the letter for {}\n \n".format(name))
     print("Dear {}, \n\n On behalf of all of us, we thank your for your generous donation of ${:10.2f}. \n You have helped make a big impact on the community!".format(name, donation))
+
+
+def show_donors(): #creating a list of donors to be displayed if "list" option selected
+    d_list = []
+    for donor in donor_db.values():
+        d_list.append(donor[0])
+    print("\n".join(d_list)) 
+
+
+def add_donor():
+    print("Adding {} to the donor list".format(name))
+    donation = input("Enter the donation amount from {}: ".format(name)) #adding donation from new donor
+    donation = float(donation) #converting to a float
+    donor_db[name] = donation #adding new donor and donation to the list
+    break
+
+def update_donor():
+    name_index = d_list.index(name.lower())
+    donation = input("Enter the new donation amount: ")
+    donation = float(donation)
+    donor_db[name_index][1].append(donation)
+    print("\n \n The {} donation from {} was added".format(donation, name))
+    break     
+
 
 #send a thank you to all donors
 def thank_you_all():
@@ -64,7 +70,7 @@ def sort_key(data):
 # create a report of donors and amounts
 def donor_report():
     spreadsheet = []
-    for (name, gifts) in donor_db:
+    for (name, gifts) in donor_db.values():
         total_given = sum(gifts)
         num_gifts = len(gifts)
         avg_gift = total_given / num_gifts
@@ -96,29 +102,29 @@ def main():
     """)
 
         selection = {
-        1: thank_you_one(), 
-        2: thank_you_all(), 
-        3: donor_report(), 
-        4: quit_program()
+        "1": thank_you_one(), 
+        "2": thank_you_all(), 
+        "3": donor_report(), 
+        "4": quit_program()
         }
         selection.get(choice, num)
-        # if response == "1":
-        #     thank_you_one()
-        # elif response == "2":
-        #     thank_you_all()    
-        # elif response == "3":
-        #     donor_report()
-        # elif response == "4":
-        #     quit_program()
-        #     break
-        # else:
-        #     print("Please select from the available options")   
+  
 
 if __name__ == "__main__": 
 
-    donor_db = [("Eliza Sommers",[4000, 250, 70]),
-                ("Tao Chien",[350, 1000, 225]), 
-                ("Joaquin Andieta",[100, 25]), 
-                ("Paulina Rodriguez",[50000]), 
-                ("Jacob Todd",[75, 80])]   
+    donor_db = {
+    'eliza_sommers': ("Eliza Sommers", [4000, 250, 70]),
+    'tao_chien': ("Tao Chien", [350, 1000, 225]), 
+    'joaquin_andieta': ("Joaquin Andieta", [100, 25]), 
+    'paulina_rodriguez': ("Paulina Rodriguez", [50000]), 
+    'jacob_todd': ("Jacob Todd", [75, 80])}
+
+
+    selection = {
+        "1": thank_you_one(), 
+        "2": thank_you_all(), 
+        "3": donor_report(), 
+        "4": quit_program()
+        }
+   
     main()             
