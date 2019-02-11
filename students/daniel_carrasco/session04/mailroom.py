821177ac932160ac01af07fbc4a591c91faa564d
dac2229 <dac2229@gmail.com>
2019-02-10 21:39:15 -0800

    how do I sort the report when using a dictinary?

--- students/daniel_carrasco/session04/mailroom.py
+++ students/daniel_carrasco/session04/mailroom.py
@@ -1,35 +1,56 @@
 #!/usr/bin/env python3
-def main():
-    print('\nChoose an action(1-4):\n\n\
-1 - Send a Thank You to a single donor.\n\
-2 - Create a Report.\n\
-3 - Send letters to all donors.\n\
-4 - Quit\n')
-    VALID_RESPONSES = (1,2,3,4)
+import tempfile
 
-    choice = int(input(''))
-    while choice not in VALID_RESPONSES:
-        print('Not a valid response, please reselect\n')
-        choice = int(input('Select 1 -4\n'))
-    arg_dict = {1:thankyou(), 2:report(), 3: letter(),4:quit()}
-    arg_dict.get(choice)
-    return None
+def main():
+    tempfile.gettempdir()
+    print(tempfile.gettempdir())
+    terminate = ''
+    while terminate != 'quit':
+        print('\nChoose an action(1 - 4):\n\n\
+                1 - Send a Thank You to a single donor.\n\
+                2 - Create a Report.\n\
+                3 - Send letters to all donors.\n\
+                4 - Quit\n')
+        VALID_RESPONSES = (1,2,3,4)
+        choice = int(input(''))
+        while choice not in VALID_RESPONSES:
+            choice = int(input('Select 1 -4\n'))
+        arg_dict = {1:thankyou, 2:report, 3: letter,4:quit}
+        arg_dict[choice]()
+
 
 def thankyou():
-    print('thank you')
+    choice = ''
+    while choice != 3 :
+        choice = int(input('\nChoose an action(1-3):\n\
+                1 - See Donor List.\n\
+                2 - Enter Name.\n\
+                3 - Quit submenu\n'))
+        if choice == 1:
+            for keys in donors.keys():
+                print(keys)
+        if choice == 2:
+            new_name = input('Enter full name\n').title()
+            donors.setdefault(new_name,[0,1])
+        if choice == 3:
+            main()
+
+    return choice
 def report():
-    print('report')
+    row_format ="{:>15}" * 4
+    print(row_format.format('Name','Donation ($)','Amount','Average ($)'))
+    for key,value in donors.items():
+        print(f'{key.title():>15}{value[0]:>15.2f}{value[1]:>15}{value[0]/value[1]:>15.2f}')
+    return None
 def letter():
-    print('letter')
+    z = 66
+    return z
 
 
 
 
 
 
-
-donors = ['art bart', 'harry scary', 'hay boo']
-cash = [1000,50,50000]
-times = [1,5,3]
 if __name__ == '__main__':
+    donors ={'art bart':[1000,1], 'harry scary':[50,5], 'hay boo':[50000,3]}
     main()
