c66903fc9507459c2a2a5689d7f4cef8c3c9f9e8
dac2229 <dac2229@gmail.com>
2019-02-11 21:57:08 -0800

    resubmitting for regrade

--- students/daniel_carrasco/session03/mailroom.py
+++ students/daniel_carrasco/session03/mailroom.py
@@ -1,35 +1,57 @@
 #!/usr/bin/env python3
+import tempfile
+from datetime import date
+import os
+
 def main():
 
-    print('\noptions\n')
-    print('A = Send a Thank You')
-    print('\n')
+    print(tempfile.gettempdir())
+    terminate = ''
+    while terminate != 'quit':
+        print('\nChoose an action(1 - 3):\n\n\
+                1 - Send a Thank You to a single donor.\n\
+                2 - Create a Report.\n\
+                3 - Quit\n')
+        VALID_RESPONSES = (1,2,3)
+        choice = int(input(''))
+        while choice not in VALID_RESPONSES:
+            choice = int(input('Select 1 -3\n'))
+        arg_dict = {1:thankyou, 2:report,3:quit}
+        arg_dict[choice]()
 
-    response = input('what do you want to do?').upper()
-    if response == 'A':
-        thankyou()
-    pass
 
 def thankyou():
-    response2 = input('Do you want to see a list?').lower()
-    if response2 == 'yes':
-        row_format ="{:>15}" * 3
-        print(row_format.format('Name','Donation','Amount'))
-        for name,amount,time in zip(donors,cash,times):
-            print(f'{name.title():>15}{"$ "+str(amount):>15}{time:>15}')
+    choice = ''
+    while choice != 3 :
+        choice = int(input('\nChoose an action(1-3):\n\
+                1 - See Donor List.\n\
+                2 - Enter Name.\n\
+                3 - Quit submenu\n'))
+        if choice == 1:
+            for keys in donors.keys():
+                print(keys)
+        if choice == 2:
+            new_name = input('Enter full name\n').title()
+            donors.setdefault(new_name,[0,1])
+        if choice == 3:
+            main()
 
-    response3 = input('Add a name?').lower()
-    if response3 == 'yes':
-        response4 = input('Add first and last').title()
-        donors.append(response4)
-        response5 = input('Donation amount?')
-        cash.append(response5)
-        times.append(1)
+    return choice
+def report():
+    row_format ="{:>15}" * 4
+    print(row_format.format('Name','Donation ($)','Amount','Average ($)'))
+    for key,value in donors.items():
+        print(f'{key.title():>15}{value[0]:>15.2f}{value[1]:>15}{value[0]/value[1]:>15.2f}')
 
-    response6 = input('Select name')
-    for num,value in enumerate(donors):
-        if response6 in value:
-            print(f'Thank you {donors[num]} for total donations of {cash[num]} with total donations at {times[num]}!')
+
+if __name__ == '__main__':
+    donors ={'art bart':[1000,1], 'harry scary':[50,5], 'hay boo':[50000,3]}
+    today = str(date.today())
+    path = os.getcwd()
+
+
+    main()
+
 
 
 
