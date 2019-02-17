ffa4576ff7dacccf478edcd660a4991a272aaf4d
admin <23247076+Rockwell70@users.noreply.github.com>
2019-02-16 18:01:34 -0800

    args_und_kwargs_function to test

--- students/jeff_shabani/session06/args_and_kwargs_lab.py
+++ students/jeff_shabani/session06/args_and_kwargs_lab.py
@@ -1,5 +1,6 @@
 def colors(fore_color='white', back_color='black', link_color='green', visited_color='blue'):
     return fore_color, back_color, link_color, visited_color
 
-test_vals = {'link_color':'red', 'back_color':'blue'}
-print(colors(**test_vals))
+
+if __name__=='__main__':
+    colors()
