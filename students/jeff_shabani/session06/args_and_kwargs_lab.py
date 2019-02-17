49adb46ca08e8ebd596d36f3709c675a0c6f4603
admin <23247076+Rockwell70@users.noreply.github.com>
2019-02-16 18:00:10 -0800

    args_und_kwargs_function to test

--- students/jeff_shabani/session06/args_and_kwargs_lab.py
+++ students/jeff_shabani/session06/args_and_kwargs_lab.py
@@ -1,22 +1,5 @@
-import unittest
+def colors(fore_color='white', back_color='black', link_color='green', visited_color='blue'):
+    return fore_color, back_color, link_color, visited_color
 
-def colors(fore_color='white', back_color = 'black', link_color = 'green', visited_color = 'blue'):
-    result = (fore_color, back_color, link_color, visited_color)
-    print(fore_color)
-
-colors('red')
-print(colors('red') == colors())
-
-class MyTests(unittest.TestCase):
-
-    # def test_short_anagram(self):
-    #     self.assertTrue(is_anagram("tea", "eat"))
-
-    def test_colors(self):
-        test_vals = ('red', 'blue', 'yellow', 'chartreuse')
-        expected = (test_vals[0], test_vals[1], test_vals[2], test_vals[3])
-        actual = colors(*test_vals)
-        self.assertEqual(colors(test_vals, expected))
-
-if __name__ == '__main__':
-    unittest.main()
+test_vals = {'link_color':'red', 'back_color':'blue'}
+print(colors(**test_vals))
