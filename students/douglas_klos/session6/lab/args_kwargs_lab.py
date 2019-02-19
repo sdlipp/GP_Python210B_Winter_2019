836645eda5b627b3b5d7451e2fb744bc19d2d6bb
Douglas Klos <dougklos@gmail.com>
2019-02-19 09:59:45 -0800

    Session6 args_kwargs_lab and unittest

--- students/douglas_klos/session6/lab/args_kwargs_lab.py
+++ students/douglas_klos/session6/lab/args_kwargs_lab.py
@@ -6,12 +6,33 @@
 # Python 210, Session 6
 # args_kwargs_lab.py
 
-def colors1(fore_color = 'blue', back_color = 'red', 
-            link_color = 'blue', visited_color = 'brown'):
-    pass
+def colors1(fore_color = 'white', back_color = 'white', 
+            link_color = 'white', visited_color = 'white'):
+    return(fore_color, back_color, link_color, visited_color)
+
+def colors2(*args, **kwargs):
+    return(args, kwargs)
+
 
 def main():
+    print(colors1())
+    print(colors1('red', 'blue', 'yellow', 'chartreuse'))
+    print(colors1(link_color = 'red', back_color = 'blue'))
+    print(colors1('purple', link_color = 'red', back_color = 'blue'))
+
+    color_tuple = ('light blue', 'grey')
+    print(colors1(*color_tuple))
+
+    color_dict = {'link_color' : 'blue', 'visited_color' : 'black'}
+    print(colors1(*color_tuple, **color_dict))
+
+    print(colors2())
+    print(colors2(*color_tuple, **color_dict))
+
+    print(colors2('red', *color_tuple, **color_dict))
+    print(colors2('red', *color_tuple, 'blue',  **color_dict))
     pass
+
 
 if __name__ == '__main__':
     main()
