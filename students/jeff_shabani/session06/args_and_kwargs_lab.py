fa00516e641909fcc1f4e97f121c7a8a76222396
Jeff Shabani <jeffrey.shabani@outlook.com>
2019-02-19 13:11:14 -0800

    Main arg & kwargs lab functions.

--- students/jeff_shabani/session06/args_and_kwargs_lab.py
+++ students/jeff_shabani/session06/args_and_kwargs_lab.py
@@ -1,22 +1,19 @@
 #!/usr/bin/env python3
 
-"""
-this is the 1st function for the first part
-of the args & kwargs lab
-"""
-
 
 def colors(fore_color='white', back_color='black', link_color='green', visited_color='blue'):
+    """
+    this is the 1st function for the first part
+    of the args & kwargs lab
+    """
     return fore_color, back_color, link_color, visited_color
 
 
-"""
-this is the 2nd function for the first part 
-of the args & kwargs lab
-"""
-
-
 def colors_two(*args, **kwargs):
+    """
+    this is the 2nd function for the first part
+    of the args & kwargs lab
+    """
     if args:
         if not kwargs:
             return (args)
