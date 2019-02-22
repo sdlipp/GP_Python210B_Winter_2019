d9ecbed50ffa71949f31c83752113f19256a26e7
dac2229 <dac2229@gmail.com>
2019-02-22 15:21:59 -0800

    made changes off of Luis feedback

--- students/daniel_carrasco/session05/mailroom.py
+++ students/daniel_carrasco/session05/mailroom.py
@@ -3,16 +3,19 @@ from datetime import date
 import os
 import sys
 
+
 def main():
     while True:
-        VALID_RESPONSES = (1,2,3,4)
+
+        VALID_RESPONSES = (1, 2, 3, 4, 5)
         try:
             user_choice = int(input('\nChoose an action(1 - 4):\n\n\
                     1 - Send a Thank You to a single donor.\n\
                     2 - Create a Report.\n\
                     3 - Send letters to all donors.\n\
-                    4 - Quit\n'))
-            arg_dict = {1:thankyou, 2:report, 3: letter,4:quit}
+                    4 - Add new donor.\n\
+                    5 - Quit\n'))
+            arg_dict = {1: thankyou, 2: report, 3: letter, 4: addnew, 5: quit}
             if arg_dict[user_choice] == 'quit':
                 sys.exit()
             else:
@@ -23,6 +26,7 @@ def main():
             print('Choice must be a menu input (1-4)')
         continue
 
+
 def thankyou():
     while True:
         try:
@@ -31,26 +35,28 @@ def thankyou():
                     2 - Enter Name.\n\
                     3 - Quit submenu\n'))
             if choice == 1:
-                for keys in donors.keys():
-                    print(keys.title())
+                [print(keys.title()) for keys in donors]
             if choice == 2:
                 new_name = input('Enter full name\n').title()
-                donors.setdefault(new_name,[0,1])
+                donors.setdefault(new_name, [0, 1])
             if choice == 3:
                 main()
         except ValueError:
             print("Input must be an integer, try again.")
             continue
 
+
 def report():
-    row_format ="{:>15}" * 4
-    print(row_format.format('Name','Donation ($)','Amount','Average ($)'))
-    for key,value in donors.items():
-        print(f'{key.title():>15}{value[0]:>15.2f}{value[1]:>15}{value[0]/value[1]:>15.2f}')
+    row_format = "{:>15}" * 4
+    print(row_format.format('Name', 'Donation ($)', 'Amount', 'Average ($)'))
+    for key, value in donors.items():
+        print(
+            f'{key.title():>15}{value[0]:>15.2f}{value[1]:>15}{value[0]/value[1]:>15.2f}')
+
 
 def letter():
-    for key,value in donors.items():
-        filename = open(key.title()+"_"+today+'.txt','w+')
+    for key, value in donors.items():
+        filename = open(key.title() + "_" + today + '.txt', 'w+')
         filename.write(f'Dear {key.title()},\n\n\
 Thank you for your very kind donation of ${value[0]}.\n\n\
 It will be put to very good use.\n\
@@ -58,10 +64,34 @@ It will be put to very good use.\n\
 \t\t-The Team')
         filename.close()
 
+
+def addnew():
+    try:
+        user_choice_name = input("Enter name of new Donor\n")
+        user_choice_donation = float(input("Enter Donation Amount\n"))
+        while True:
+            user_confirm = input(
+                f'Add {user_choice_name:10} with a donation of ${user_choice_donation:10.2f}?\n\n Enter (Y/N)')
+            try:
+                if user_confirm == 'Y':
+                    donors[user_choice_name] = [user_choice_donation, 1]
+                    break
+                else:
+                    user_choice_name = input("Enter name of new Donor\n")
+                    user_choice_donation = float(
+                        input("Enter Donation Amount\n"))
+            except ValueError:
+                print("Input must be an integer, try again.")
+
+    except ValueError:
+        print("Input must be an integer, try again.")
+
+
 if __name__ == '__main__':
-    donors ={'art bart':[1000,1], 'harry scary':[50,5], 'hay boo':[50000,3]}
+    donors = {
+        'art bart': [1000, 1], 'harry scary': [50, 5], 'hay boo': [50000, 3]
+    }
     today = str(date.today())
     path = os.getcwd()
-
 
     main()
