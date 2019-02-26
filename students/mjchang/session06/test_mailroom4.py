b7cf1c8abbdadc103159b34281a72d8242f6ceb3
mjchang14 <mjchang14@gmail.com>
2019-02-25 22:02:05 -0800

    got test_create_report to work

--- students/mjchang/session06/test_mailroom4.py
+++ students/mjchang/session06/test_mailroom4.py
@@ -1,56 +1,69 @@
 #!/usr/bon/env python3
 
-
 import os
 import sys
+import datetime
 import unittest
 
-import mailroom4
+import mailroom4 as mr4
 
-mr4.donor_db = mailroom4.donor_db()
+mr4.donor_db = mr4.donor_db()
 
 def test_donor_list():
     """ test to see if donors are showing up in list"""
     donors = mr4.donor_db.keys()
-    assert len(donors.split('\n')) == 5
+    assert len(donors) == 5
     assert "Tao Chien" in donors
     assert "Eliza Sommers" in donors
 
 
 def test_no_donor_search():
     """ test that a new donor doesn't show up"""
-    donor = mr4.donor_db.keys()
+    donor = mr4.donor_db.get("Mama Fresia")
     assert donor is None
 
 
-
 def test_donor_search():
-    """ test that an existing donor is there
-also test if existing donor can be found using different capitalization inputs"""
-    donor = mr4.donor_db.keys()
-    assert donor[2] == "Joaquin Andieta"
-    assert donor[4] == "Jacob Todd"
-
+    """ test that an existing donor is there"""
+    donors = mr4.donor_db.keys()
+    assert "Joaquin Andieta" in donors
 
 
 def test_gen_single_letter():
-    """test that the donor letter/message is generated"""
-    pass
+    """test that the donor letter/message is generated
+    bring in a test donor, then check the letter"""
+    expected = "Dear Jacob Todd, \n\n Thank you for your generous donation of $    100.00. \n You have helped make a big impact on the community!"
+    assert mr4.generate_letter("Jacob Todd", 100.00) == expected
+    
 
 
 def test_donor_addition():
-    """ test that new donor is added"""
-    pass
+    """ test that new donor is added
+    created a test new donor and donation, then check that new donor is in the donor_db"""
+    name = "Isabel Allende"
+    donation = 2500
+    mr4.donor_db.setdefault(name, []).append(donation)
+    donors = mr4.donor_db.keys()
+    don_amt = mr4.donor_db.values()
+    assert "Isabel Allende" in donors
+    assert mr4.donor_db.get("Isabel Allende") == [2500]
 
 
 def test_create_report():
-    """ test that the donor report is created and displayed correctly"""
-    pass
+    """ test that the donor report is created
+    using a random line from the report to test that it displays correctly"""
+    expected = "Paulina Rodriguez               $    50000.00                 1  $    50000.00"
+    assert mr4.report_line(["Paulina Rodriguez", 50000, 1, 50000]) == expected
 
 
 def test_gen_letters():
-    """ test that letters were generated and saved to the specified folder"""
-    pass
+    """ test that letters were generated to separate files"""
+    timestamp = str(datetime.date.today())
+    assert os.path.isfile('Tao Chien_'+timestamp+'.txt')
+    assert os.path.isfile('Joaquin Andieta_'+timestamp+'.txt')
+
+def test_get_letter_text():
+    expected = ""
 
 
 if __name__ == "__main__":
