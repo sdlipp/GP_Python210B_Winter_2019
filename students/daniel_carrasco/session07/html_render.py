68c49b89216e458114646af9aa0f97a25154e4e6
dac2229 <dac2229@gmail.com>
2019-03-02 14:33:03 -0800

    step 1

--- students/daniel_carrasco/session07/html_render.py
+++ students/daniel_carrasco/session07/html_render.py
@@ -4,15 +4,28 @@
 A class-based system for rendering html.
 """
 
-
 # This is the framework for the base class
 class Element(object):
 
     def __init__(self, content=None):
-        pass
+        "initializes list, checks to see if any content is sent, if yes append"
+        self.content = []
+        self.htmlstart = "<html>\n"
+        self.htmlend = "</html>"
+        if content is not None:
+            self.content.append(content)
 
     def append(self, new_content):
-        pass
+        "appends content with new_content"
+        self.content.append(new_content)
 
     def render(self, out_file):
-        out_file.write("just something as a place holder...")
+        out_file.write(f'{self.htmlstart}')
+        for line in self.content:
+            out_file.write(line+'\n')
+        out_file.write(f'{self.htmlend}')
+
+page = Element("Some content")
+page.append("Some more contenet")
+with open("test.html", 'w') as outfile:
+    page.render(outfile)
