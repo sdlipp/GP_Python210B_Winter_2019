2e7dbf1ca6a9c696d34993780d5a300fab3dbc5c
UncleanlyCleric <junyatwin@gmail.com>
2019-03-04 17:11:23 -0800

    Initial commit

---
+++ students/jesse_miller/session08/circle.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+'''
+Class based circle calculation exercise
+'''
+
+'''
+Setting this space for the modules I'm certainly importing
+'''
+
+class Circle:
+    def __init__(self, radius):
+        self.radius = radius 
