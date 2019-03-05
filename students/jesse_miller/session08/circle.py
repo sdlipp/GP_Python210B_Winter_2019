83126b40d8369bc5aad422ff3979e7942d56ff99
UncleanlyCleric <junyatwin@gmail.com>
2019-03-05 08:06:08 -0800

    Yea, I'm still not sure I get OO programming at all.  This is very complicated to me.

--- students/jesse_miller/session08/circle.py
+++ students/jesse_miller/session08/circle.py
@@ -3,10 +3,35 @@
 Class based circle calculation exercise
 '''
 
+from math import pi
 '''
-Setting this space for the modules I'm certainly importing
+Imported the math module for calculation
 '''
 
-class Circle:
+class Circle(object):
+    '''
+    Defining the circle class
+    '''
     def __init__(self, radius):
-        self.radius = radius 
+        self.radius = radius
+
+    @property
+    def diameter(self):
+        '''
+        Defining diameter, I think I'm doing this right.
+        '''
+        return self.radius * 2
+
+    @diameter.setter
+    def diameter(self, value):
+        '''
+        This seems like the right time to use a setter
+        '''
+        self.radius = value / 2
+
+    @property
+    def area(self):
+        '''
+        Defining area, I'm a tad more confident in this one
+        '''
+        return pi * self.radius**2
