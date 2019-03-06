f1e90c5b6c06e4c034da5482ebf7d5a3a475080f
admin <23247076+Rockwell70@users.noreply.github.com>
2019-03-05 19:43:09 -0800

    circle class

--- students/jeff_shabani/session08/Circle.py
+++ students/jeff_shabani/session08/Circle.py
@@ -1,16 +1,29 @@
-
-
-
-
+#!/usr/bin/env python3
 
 class Circle(object):
     def __init__(self, radius):
         self.radius = radius
+        self.diameter = radius * 2
+
+    @property
+    def diameter(self):
+        return self.radius * 2
+
+    @diameter.setter
+    def diameter(self, value):
+        self.diameter = value
+
+    #@diameter.getter
+
 
     def __repr__(self):
-        return f'The radius is {self.radius}'
+        return f'The radius is {self.radius} & diamer is {self.diameter}'
 
 
 if __name__ =='__main__':
     c = Circle(5)
+
     print(c)
+
+    c.diameter = 20
+    print(c.diameter)
