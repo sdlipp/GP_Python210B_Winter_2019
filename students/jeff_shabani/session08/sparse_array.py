0b269ba66223f588f57ae210ee3d1a0b7840b29d
JRockwell70 <rockwellshabani@gmail.com>
2019-03-09 11:05:52 -0800

    sparse_array updates

--- students/jeff_shabani/session08/sparse_array.py
+++ students/jeff_shabani/session08/sparse_array.py
@@ -2,30 +2,62 @@
 
 class SparseArray(object):
 
-    def __init__(self, sequence, *seq_dict):
+    def __init__(self, sequence=None):
+        if sequence is None:
+            self.sequence = []
         self.sequence = sequence
-        self.seq_dict = dict(seq_dict)
+        self.display_list = [i for i in self.sequence if i >0]
+        self.working_dict = {key: value for key, value in enumerate(self.sequence) if value > 0}
 
-    def dict_convert(self):
-        #seq_dict = dict()
-        for k,v in enumerate(self.sequence):
-            self.seq_dict[k]=v
-        return self.seq_dict
-
+    def create_dictionary(self):
+        self.working_dict = {key: value for key, value in enumerate(self.sequence) if value > 0}
 
     def __len__(self):
         return len(self.sequence)
 
+    def append(self, val):
+        if val == 0:
+            return self.sequence
+        self.sequence.append(val)
+
+        self.working_dict = {key: value for key, value in enumerate(self.sequence) if value > 0}
+
+
+    def __delitem__(self, key):
+        del (self.working_dict[key])
+        self.sequence.pop(key)
+
+    def __getitem__(self, item):
+        if item not in self.working_dict.keys():
+            return 0
+        else:
+            return self.working_dict[item]
+
     def __setitem__(self, key, value):
         self.sequence[key] = value
 
+    def __contains__(self, item):
+        if self.sequence:
+            return True
 
     def __repr__(self):
-        return f'{self.seq_dict.values()} has a length of {len(self.sequence)}'
+        return f'{self.sequence} has a length of {len(self.sequence)}'
 
-spa = SparseArray([1,2,3])
 
-print(len(spa))
+if __name__ == '__main__':
 
-spa[1] = 2
-print(spa)
+    spa = SparseArray([1, 0, 2, 3])
+    print(spa)
+    print(len(spa))
+
+    spa.append(10)
+    print(spa)
+
+    print(spa[2])
+    del(spa[4])
+    print(spa)
+
+    # spa[5]=10
+    # print(spa)
+    # print(type(spa))
+    # print(10 in spa)
--- students/jeff_shabani/session08/test_sparse_array.py
+++ students/jeff_shabani/session08/test_sparse_array.py
@@ -8,7 +8,20 @@ class SparseArrayTest(unittest.TestCase):
 
     def test_init(self):
         spa = SparseArray([1, 2, 3])
-        self.assertEqual(repr(spa), '[1, 2, 3]')
+        self.assertEqual(repr(spa), '{0: 1, 1: 2, 2: 3} has a length of 3')
+        self.assertIsInstance(spa, SparseArray)
+        del spa
+
+    def test_length(self):
+        spa = SparseArray([1,2,3,4])
+        self.assertEqual(len(spa), 4)
+        del spa
+
+    def test_append(self):
+        spa = SparseArray([1, 2, 3, 4])
+        spa[4]=10
+        self.assertIn(10, spa)
+
 
 
 if __name__ == '__main__':
