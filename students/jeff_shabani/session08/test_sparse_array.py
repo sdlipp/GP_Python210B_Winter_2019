d8b513e591f3a6d30cd73d8915424ce219c8dcca
JRockwell70 <rockwellshabani@gmail.com>
2019-03-10 16:32:53 -0700

    sparse_array updates

--- students/jeff_shabani/session08/test_sparse_array.py
+++ students/jeff_shabani/session08/test_sparse_array.py
@@ -8,7 +8,24 @@ class SparseArrayTest(unittest.TestCase):
 
     def test_init(self):
         spa = SparseArray([1, 2, 3])
-        self.assertEqual(repr(spa), '[1, 2, 3]')
+        self.assertEqual(repr(spa), '[1, 2, 3] has a length of 3')
+        del spa
+
+    def test_get_length(self):
+        spa = SparseArray([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
+        self.assertEqual(len(spa), 10)
+        del spa
+
+    def test_append(self):
+        spa = SparseArray([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
+        spa.append(11)
+        self.assertEqual(len(spa), 11)
+        del spa
+
+    def del_item_from_index(self):
+        spa = SparseArray([1, 0, 2, 3])
+        del(spa[1])
+        self.assertEqual(spa.sequence, [1, 2, 3])
 
 
 if __name__ == '__main__':
