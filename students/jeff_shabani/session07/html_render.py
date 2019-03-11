79468e6dc1cdc78f99e221e8099608927b1e8911
Jeff Shabani <jeffrey.shabani@outlook.com>
2019-03-11 08:01:21 -0700

    html_render update

--- students/jeff_shabani/session07/html_render.py
+++ students/jeff_shabani/session07/html_render.py
@@ -5,13 +5,14 @@ A class-based system for rendering html.
 """
 import functools
 
+
 # This is the framework for the base class
 class Element(object):
-    indent = ' ' * 2
+    indent = ''
     tag = 'html'
 
-    def __init__(self, content=None, **attrs):
-        self.attrs = attrs
+    def __init__(self, content=None, **kwargs):
+        self.attrs = kwargs
         if content:
             self.content = [content]
         else:
@@ -28,15 +29,15 @@ class Element(object):
     def append(self, new_content):
         self.content.append(new_content)
 
-    def render(self, file_name, cur_indent=''):
+    def render(self, file_name, indent='', ind_count=0):
         # Writes the opening tag
         file_name.write(f'{self._front_tag()}\n')
 
         for content_line in self.content:
             if hasattr(content_line, 'render'):
-                content_line.render(file_name)
+                content_line.render(file_name, indent, ind_count + 1)
             else:
-                file_name.write(f'{content_line}\n')
+                file_name.write(f'{self.indent}{indent}{content_line}\n')
         file_name.write(f'{self._end_tag()}\n')
 
 
@@ -46,9 +47,10 @@ class Html(Element):
     """
     tag = 'html'
 
-    def render(self, file_name):
+    def render(self, file_name, indent="", ind_count=0):
+        self.indent = indent
         file_name.write(f'<!DOCTYPE html>\n')
-        super().render(file_name)
+        super().render(file_name, indent)
 
 
 class P(Element):
@@ -68,20 +70,23 @@ class Body(Element):
 """
 Step 3: print on one line
 """
-class OneLineTag(Element):
 
+
+class OneLineTag(Element):
     tag = 'Title'
 
-    def render(self, file_name, cur_indent=''):
+    def render(self, file_name, indent="", ind_count=0):
         """
         Renders elements on a single line.
-        :param file_name:
-        :param cur_indent:
+        :param file_name: Rendered object destination
+        :param indent: Indentation level
+        :param ind_count: Number of times indentation is to be applied
         """
-        file_name.write(f'{self._front_tag()} ')
+        self.indent = indent * ind_count
+        file_name.write(f'{self.indent}{self._front_tag()[:-1]} ')
         for k, v in self.attrs.items():
             file_name.write(f'{k}="{v}"')
-        file_name.write(f'{self._front_tag()}')
+        file_name.write(f'>')
         file_name.write(f'{self.content[0]}')
         file_name.write(f'{self._end_tag()}')
 
@@ -90,84 +95,101 @@ class OneLineTag(Element):
 
 
 class Head(Element):
-
     tag = "head"
-
 
 
 """
 Step 5: Self closing tag
 """
 
+
 class SelfClosingTag(Element):
-    tag = 'br'
+
+    def __init__(self, content=None, **kwargs):
+
+        if content: raise TypeError
+        self.attrs = kwargs
 
     def append(self, new_content):
         raise NotImplementedError
 
-    def render(self, file_name):
+
+    def render(self, file_name, indent="", ind_count=0):
 
         """
-        if conent is entered this tells user that self closing tags
+        if content is entered this tells user that self closing tags
         can't have conent and resets the conent to an empty string.
         """
 
+        self.indent = indent * ind_count
+
         if self.content:
             raise TypeError
-        file_name.write(f'{self._front_tag()[:-1]}')
+        file_name.write(f'{self.indent}{self._front_tag()[:-1]}')
         for k, v in self.attrs.items():
             file_name.write(f'{k.rjust(len(k) + 1)}="{v}" />')
+
+
+
 
 """
 Step 6
 """
 
-class A(OneLineTag):
 
+class A(OneLineTag):
     tag = 'a'
 
-    def __init__(self, link, content=None, **attrs):
-        if not (content and link): raise TypeError
+    def __init__(self, link, content=None, **kwargs):
+        if not (content and link):
+            raise TypeError
 
-        attrs['href'] = link
-        super().__init__(content, **attrs)
+        kwargs['href'] = link
+        super().__init__(content, **kwargs)
 
-    # def __init__(self, link, content):
-    #     self.link = link
-    #     self.content = content
-    #     #super(Element).__init__()
-    #
-    # def render(self, file_name):
-    #     filename.write()
-    #     head = 'a href='
-    #     tail = 'a'
-    #     outtext = f'<{head}"{self.link}">{self.content}</{tail}>'
-    #     with open(f'{file_name}.html', 'w') as file:
-    #         file.write(outtext)
-#
-# class Ul(Element):
-#     """
-#     Step 7: Ul class
-#     """
-#     ul = []
-#
-# class Li(Element):
-#     """
-#     Step 7: Li class
-#     """
-#     list_element = ''
-#
 
-#
-# class Meta(SelfClosingTag):
-#     """
-#     add meta tag
-#     """
-#
-#     def __init__(self, content=None, tag = 'meta charset="UTF-8"'):
-#         super().__init__(content, tag)
+class Hr(SelfClosingTag):
 
-# if __name__ == '__main__':
-#
-#     olt = OneLineTag('this is william')
-#     olt.render(olt, 'tag')
+    tag = 'hr'
+
+
+# <br /> is XHTML format
+class Br(SelfClosingTag):
+    tag = 'br'
+
+    def __init__(self, content=None, **kwargs):
+        """
+        __init__ method for Br class.
+        Checks that no attributes were passed then calls super().__init__
+        :param content: Content that is to be added to the instance content and later rendered.
+        :param kwargs:  br elements accept no attributes.  Raises TypeError is passed in.
+        """
+        if kwargs: raise TypeError
+
+        super().__init__(content, **kwargs)
+
+
+class Ul(Element):
+    """
+    Step 7: Ul class
+    """
+    tag = 'ul'
+
+    def __init__(self, content=None, **kwargs):
+        self.content = []
+        self.attrs = kwargs
+
+
+class Li(Element):
+    """
+    Step 7: Li class
+    """
+    list_element = ''
+
+
+class Meta(SelfClosingTag):
+    """
+    add meta tag
+    """
+
+    tag = 'meta'
