6f409c07f049e021cf53df7212de711793cc6b20
Jeff Shabani <jeffrey.shabani@outlook.com>
2019-03-11 15:13:13 -0700

    donor_collection

---
+++ students/jeff_shabani/session09/donor_collection.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+
+"""
+Framework for individual donors.
+"""
+
+# Jeff Shabani
+# March 1st, 2019
+# Python 210, Session 9
+# donors.py
+
+class DonorCollection():
+    def __init__(self, name):
+        self.name = name
+        self.donation = 0
+
+    donors = {'William B': [120, 130, 50],
+              'Sammy Maudlin': [500, 125, 670, 1000],
+              'Bobby Bittman': [10],
+              'Skip Bittman': [75, 125, 19],
+              'Ashley Lashbrooke': [10000, 15000]}
