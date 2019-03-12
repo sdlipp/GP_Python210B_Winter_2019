029d97d85a70ffa57335505a3285ddb70f6b9862
JRockwell70 <rockwellshabani@gmail.com>
2019-03-11 17:48:19 -0700

    oo_mailroom updates

--- students/jeff_shabani/session09/donor_collection.py
+++ students/jeff_shabani/session09/donor_collection.py
@@ -1,7 +1,13 @@
 #!/usr/bin/env python3
 
+from collections import OrderedDict
+from operator import itemgetter
+from pathlib import Path
+
+from write_a_letter import write_a_letter
+
 """
-Framework for individual donors.
+Framework accessing multiple donors.
 """
 
 # Jeff Shabani
@@ -10,12 +16,33 @@ Framework for individual donors.
 # donors.py
 
 class DonorCollection():
-    def __init__(self, name):
-        self.name = name
-        self.donation = 0
 
     donors = {'William B': [120, 130, 50],
               'Sammy Maudlin': [500, 125, 670, 1000],
               'Bobby Bittman': [10],
               'Skip Bittman': [75, 125, 19],
               'Ashley Lashbrooke': [10000, 15000]}
+
+    def __init__(self, name=''):
+        self.name = name
+        self.donation = 0
+
+    def view_donor_names(self):
+        [print(name) for name in self.donors]
+
+    def create_new_donors_dict(self):
+        """
+        dictionay comprehension of donors with sum, len, and average of values.
+        """
+        new_donors = {k: (sum(v), len(v), (len(v) / len(v))) for k, v in self.donors.items()}
+        return OrderedDict(sorted(new_donors.items(), key=itemgetter(1), reverse=True))
+
+    def write_letters_to_all_donors(self):
+        #TODO: set_letter_directory_path_path()
+        for donor, total in self.create_new_donors_dict().items():
+            with open(f'{donor}.txt', 'wt') as letter:
+                letter.write(write_a_letter(donor, total[0]))
+
+
+
+
