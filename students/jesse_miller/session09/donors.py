07a3fa7a880362544cf76df3550e1e20c9cca92a
UncleanlyClericr <junyatwin@gmail.com>
2019-03-12 08:29:38 -0700

    First try through of the donors.py files.  Will start writing tests in a few to see if they work.

--- students/jesse_miller/session09/donors.py
+++ students/jesse_miller/session09/donors.py
@@ -4,75 +4,100 @@ This is where we start.  Building out the donor database and manipulation
 Everything else in this program will be dependent on this one.
 '''
 class Donor():
-
+    '''
+    Hello, I am new.  Anything below "donors =" is old and can be ignored
+    '''
     def __init__(self, name):
         self.name = name
-        self.donation = {}
+        self.donation = []
 
-donors = {'Robert Smith': [435.56, 125.23, 357.10],
-          'JD Cronise': [123.12],
-          'Chris Stapleton': [243.87, 111.32],
-          'Dave Lombardo': [63.23, 422.87, 9432.01],
-          'Randy Blythe': [223.50, 8120.32],
-          'Devin Townsand': [431.12, 342.92, 5412.45],
-          }
+    def donation_add(self, new_don):
+        '''
+        Adding donations
+        '''
+        self.donation.append(new_don)
 
+    @property
+    def num_dons(self):
+        '''
+        Number of donations made
+        '''
+        return len(self.donation)
 
-def donor_list():
-    '''
-    This when done properly, will print the list of donor names
-    '''
-    print(f"\n{'-'*15}\nList of Donors:\n{'-'*15}")
-    for donor in donors:
-        print(donor)
-    print(f"{'-'*15}\n")
+    @property
+    def total_dons(self):
+        '''
+        Total of donations
+        '''
+        return sum(self.donation)
 
-
-def donor_report():
-    '''
-    This will be the donation report section
-    '''
-    summary = []
-    headers = ['Donor Name', 'Total Given', 'Times Donated', 'Average Gift']
-
-    print(f"\n{'-'*80}\n{{:17}} | {{:<19}} | {{:<15}} | {{:<19}}\n{'-'*80}"\
-    .format(headers[0], headers[1], headers[2], headers[3]))
-
-    for k, v in donors.items():
-        summary.append([k, (sum(v)), (len(v)), (sum(v) / len(v))])
-    summary.sort(key=lambda d: d[1], reverse=True)
-
-    for x_value in summary:
-        print('{:17} | ${:<18,.2f} | {:<15} | ${:<16,.2f}'.format
-              (x_value[0], x_value[1], x_value[2], x_value[3]))
-    print(f"{'-'*80}\n")
-
-
-def donor_add(current_donor):
-    '''
-    This allows addition of new donors
-    '''
-    donors[current_donor] = []
-    while True:
+    @property
+    def avg_don(self):
+        '''
+        Average of donations
+        '''
         try:
-            d_num = int(input('How many donations were made: '))
-            while d_num > 0:
-                new_don = float(input('Enter their donation: '))
-                donors[current_donor].append(new_don)
-                d_num -= 1
-            mail_send(current_donor)
-        except (KeyboardInterrupt, EOFError, ValueError):
-            break
+            return self.total_dons / self.num_dons
+        except ZeroDivisionError:
+            return 0
 
 
-def donor_del():
-    '''
-    This section allows the user to delete a donor
-    '''
-    try:
-        donor_list()
-        del_donor = str(input('Enter the name of the donor to remove: '))
-        del donors[del_donor]
-        donor_list()
-    except (KeyboardInterrupt, EOFError, ValueError):
-        safe_input()
+class DonorListings:
+    """
+    This is what contains the donor lists, and allows us to query for single
+    or total donors.  This is also where the report function resides.
+    """
+
+    def __init__(self):
+        self.donors_dict = {'Robert Smith': [435.56, 125.23, 357.10],
+                            'JD Cronise': [123.12],
+                            'Chris Stapleton': [243.87, 111.32],
+                            'Dave Lombardo': [63.23, 422.87, 9432.01],
+                            'Randy Blythe': [223.50, 8120.32],
+                            'Devin Townsand': [431.12, 342.92, 5412.45],
+                           }
+
+    def donor_creation(self, donor_name):
+        '''
+        Here we can create a new donor
+        '''
+        new_donor = Donor(donor_name)
+        self.donors_dict[donor_name] = new_donor
+        return new_donor
+
+    def donor_find(self, donor_name):
+        '''
+        This is the search function for donors
+        '''
+        try:
+            return self.donors_dict[donor_name]
+        except KeyError:
+            return self.donor_creation(donor_name)
+
+    def donor_list(self):
+        '''
+        Finally here is our listing function for donors
+        '''
+        donor_list = [donor.name for donor in self.donors_dict.values()]
+        sorted_donors = sorted(donor_list)
+        return sorted_donors
+
+    @staticmethod
+    def sort_key(donor_stats):
+        '''
+        This is how we will sort our donors.  This uses the first field (total)
+        as the sort key
+        '''
+        return donor_stats[1]
+
+    def create_report(self):
+        """
+        Creates a report for all donors in the list.  This sorts by total
+        donated.
+        """
+        summary = [[donor.name, donor.total_dons, donor.num_dons, donor.avg_don]
+                   for donor in self.donors_dict.values()]
+
+        sorted_summary = sorted(summary, key=self.sort_key, reverse=True)
+
+        return sorted_summary
