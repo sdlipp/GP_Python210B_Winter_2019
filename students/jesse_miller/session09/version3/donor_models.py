cdd159b45bb381ca82d383de6610e4ab11ba96b2
UncleanlyClericr <junyatwin@gmail.com>
2019-03-13 08:38:36 -0700

    So far, so good, actually...

--- students/jesse_miller/session09/version3/donor_models.py
+++ students/jesse_miller/session09/version3/donor_models.py
@@ -1,1 +1,63 @@
 #!/usr/bin/env python3
+'''
+Let's try this by following the directions.  So, this is going to be where the
+donor manipulation occurs.  The goal is to compartmentalize everything so that
+functions can be called by other programs if and when needed.
+'''
+import datetime
+
+
+class Donor:
+    '''
+    This will be slightly easier in a sense because we're only dealing with a
+    single donor.  However, I get to look forward to pairing everything down to
+    an interesting, if not insane, degree from previous attempts.
+    '''
+
+    def __init__(self, name):
+        self.name = name
+        self.donations = [] #?  We'll try it.
+
+    def donation_add(self, new_donation):
+        '''
+        This should append donations to the list
+        '''
+        self.donations.append(new_donation)
+
+    def donation_count(self):
+        '''
+        Fairly obvious, counts the number of donations
+        '''
+        return len(self.donations)
+
+    def donation_total(self):
+        '''
+        Again, obvious, adds the donations up
+        '''
+        return sum(self.donations)
+
+    def donation_average(self):
+        '''
+        Averages the donations
+        '''
+        return self.donation_total / self.donation_count
+
+    def letter_template(self):
+        date = datetime.datetime.now().strftime("%B %d, %Y")
+        template = ('\n {date} \n'
+                    '\nHello {}, \n'
+                    '\n'
+                    'We are writing to thank you for you generous donation\n'
+                    'to our foundation.  Your contributions for the year \n'
+                    'total ${:,.2f} in {} disbursements. \n'
+                    '\n'
+                    'Again, the foundation thanks you for your support, \n'
+                    'and we hope to remain in contact with you in this new \n'
+                    'year.\n'
+                    '\n'
+                    'Sincerely, \n'
+                    'Ecumenical Slobs LLC \n')
+        return template
+
+
+#class DonorCollection:
--- students/jesse_miller/session09/version3/test_mailroom_oo.py
+++ students/jesse_miller/session09/version3/test_mailroom_oo.py
@@ -2,10 +2,18 @@
 '''
 Here we go testing again!
 '''
-from donor_models import Donor, DonorCollection
+from donor_models import Donor
 import cli_main
 
 
 import pytest
 import datetime
 import os
+
+'''
+We'll start with testing the donor class
+'''
+def test_donor_init():
+    donor = Donor("James Hetfield")
+    assert donor.name == "James Hetfield"
+    assert donor.donations == []
