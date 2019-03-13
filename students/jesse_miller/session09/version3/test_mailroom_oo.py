c6e55ee2fc5a7dd9ddf38d42c40d1c914403fbae
UncleanlyClericr <junyatwin@gmail.com>
2019-03-13 09:08:31 -0700

    3 down.

--- students/jesse_miller/session09/version3/test_mailroom_oo.py
+++ students/jesse_miller/session09/version3/test_mailroom_oo.py
@@ -2,14 +2,12 @@
 '''
 Here we go testing again!
 '''
-from donor_models import Donor
-import cli_main
-
-
-import pytest
 import datetime
 import os
+import pytest
 
+from donor_models import Donor
+import cli_main
 '''
 We'll start with testing the donor class
 '''
@@ -21,6 +19,7 @@ def test_donor_init():
     assert donor.name == 'James Hetfield'
     assert donor.donations == []
 
+
 def test_donor_init_error():
     '''
     Now, let's see if we can break it
@@ -28,3 +27,16 @@ def test_donor_init_error():
     with pytest.raises(TypeError):
         #pylint: disable=E1121
         Donor('Kirk Hammett', 400)
+
+
+def test_donation_add():
+    '''
+    Let's try to add money the right way
+    '''
+    donor = Donor('James Hetfield')
+    donor.donation_add(400)
+
+    assert donor.donations == [400]
+
+    donor.donation_add(3000)
+    assert donor.donations == [400, 3000]
